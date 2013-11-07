79a51d0887de691599d25749f0945c2479279119
Andrés Pereira <apereira@alabs.es>
2013-11-07 10:50:30 +0100

    geoplanet woeid test

--- Gemfile
+++ Gemfile
@@ -43,3 +43,6 @@ end
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
+
+gem 'geoplanet'
+gem 'mysql2'
--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,2 +1,19 @@
 class Ad < ActiveRecord::Base
+
+  require 'geoplanet'
+
+  class << self
+ 
+    def search_by_woeid woeid
+      GeoPlanet.appid = APP_CONFIG[:geoplanet_app_id]
+      GeoPlanet::Place.new(woeid)
+    end
+
+    def filter_by_woeid woeid
+      GeoPlanet.appid = APP_CONFIG[:geoplanet_app_id]
+      Ad.find_by_woeid_code(GeoPlanet::Place.new(woeid))
+    end
+
+  end
+
 end
