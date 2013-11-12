eed222bd8ad8f9c173f642248ddfe77d0d07d8b7
Andrés Pereira <apereira@alabs.es>
2013-11-12 16:43:48 +0100

    fix: geo bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,9 @@ class ApplicationController < ActionController::Base
 
   def get_location_suggest 
     ip_address = Rails.env == "development" ? "87.218.80.79" : request.remote_ip
-    ip_address = request.env['HTTP_X_FORWARDED_FOR'] || ip_address
+    if request.env['HTTP_X_FORWARDED_FOR']
+      ip_address = request.env['HTTP_X_FORWARDED_FOR'].split(',')[0] || ip_address
+    end
     GeoHelper.suggest ip_address
   end
 
--- app/helpers/geo_helper.rb
+++ app/helpers/geo_helper.rb
@@ -4,12 +4,7 @@ module GeoHelper
    def self.suggest ip_address
      c = GeoIP.new('vendor/geolite/GeoLiteCity.dat').country(ip_address)
      # cutre translator from Spain to España
-     case c.country_name
-     when "Spain"
-       country = "España"
-     else
-       country = c.country_name
-     end
+     country = c.country_name == "Spain" ? "España" : c.country_name
      "#{c.city_name}, #{c.real_region_name}, #{country}".force_encoding('utf-8')
    end
 
