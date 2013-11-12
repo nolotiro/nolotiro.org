b853ec30897e320342a0541fedb591f6b4e9fee7
Andrés Pereira <apereira@alabs.es>
2013-11-12 12:21:01 +0100

    capistrano geolite

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :deploy_via, :remote_cache
 
 set :ssh_options, { :forward_agent => true }
 
-set :linked_files, %w{config/database.yml config/app_config.yml}
+set :linked_files, %w{config/database.yml config/app_config.yml vendor/geolite/GeoLiteCity.dat}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
