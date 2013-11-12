67d77b7bd6fbdcda10dba02ee6859d496c78f953
Andrés Pereira <apereira@alabs.es>
2013-11-12 09:27:40 +0100

    capistrano

--- Gemfile
+++ Gemfile
@@ -39,7 +39,7 @@ end
 # gem 'unicorn'
 
 # Use Capistrano for deployment
-# gem 'capistrano', group: :development
+gem 'capistrano', group: :development
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
---
+++ config/deploy.rb
@@ -0,0 +1,49 @@
+set :application, 'nolotiro.org'
+set :repo_url, 'git@github.com:alabs/nolotiro.org.git'
+
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+set :deploy_to, '/var/www/beta.nolotiro.org'
+set :scm, :git
+
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
+
+#set :deploy_via, :copy
+set :deploy_via, :remote_cache
+
+set :ssh_options, { :forward_agent => true }
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :keep_releases, 5
+
+server "beta.nolotiro.org", roles: [:app, :web, :db]
+
+namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
+
+end
+
+
