7435461770ce532d8d656fff4172492cbf781726
Andrés Pereira <apereira@alabs.es>
2013-11-13 16:28:05 +0100

    ads: migrate status from string with values available, booked and delivered to 1, 2 and 3

---
+++ db/migrate/20131113104108_change_status_type_to_ads.rb
@@ -0,0 +1,15 @@
+class ChangeStatusTypeToAds < ActiveRecord::Migration
+  def self.up
+    Ad.update_all("status = 1", ['status = "available"'])
+    Ad.update_all("status = 2", ['status = "booked"'])
+    Ad.update_all("status = 3", ['status = "delivered"'])
+    change_column :ads, :status, :integer, :default => 1
+  end
+
+  def self.down
+    change_column :ads, :status, :string, :default => "available"
+    Ad.update_all("status = 'available'", ["status = 1"])
+    Ad.update_all("status = 'booked'", ["status = 2"])
+    Ad.update_all("status = 'delivered'", ["status = 3"])
+  end
+end
