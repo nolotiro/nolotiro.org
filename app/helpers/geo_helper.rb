14941aa9377be09e293ae855966627d19f8002b5
Andrés Pereira <apereira@alabs.es>
2013-11-17 13:43:40 +0100

    test: geohelper

--- app/helpers/geo_helper.rb
+++ app/helpers/geo_helper.rb
@@ -2,7 +2,7 @@
 module GeoHelper
 
   def self.get_ip_address request
-    ip_address = Rails.env == "development" ? "87.218.80.79" : request.remote_ip
+    ip_address = ["test", "development"].includes? Rails.env  ? "87.218.80.79" : request.remote_ip
     if request.env['HTTP_X_FORWARDED_FOR']
       ip_address = request.env['HTTP_X_FORWARDED_FOR'].split(',')[0] || ip_address
     end
--- test/helpers/geo_helper_test.rb
+++ test/helpers/geo_helper_test.rb
@@ -1,4 +1,30 @@
 require 'test_helper'
 
 class GeoHelperTest < ActionView::TestCase
+  include GeoHelper
+
+  test "should_get_ip_address" do
+    request = {}
+    ip = GeoHelper.get_ip_address request
+    assert_equal(ip, "87.218.80.79")
+  end 
+
+  test "should_get_ip_address_with_http_x_forwared_for" do
+    # TODO
+    request = {}
+    ip = GeoHelper.get_ip_address request
+    assert_equal(ip, "87.218.80.79")
+  end 
+
+  test "should_get_ip_address_with_remote_ip" do
+    # TODO
+    request = {}
+    ip = GeoHelper.get_ip_address request
+    assert_equal(ip, "87.218.80.79")
+  end 
+
+  test "should_suggest_location_with_ip_address" do
+    # TODO
+  end
+
 end
