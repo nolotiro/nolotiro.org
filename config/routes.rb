762b762f5a897eef855c13cb0e320ab2f79437a2
Andrés Pereira <apereira@alabs.es>
2013-11-17 13:36:27 +0100

    messages: routes

--- config/routes.rb
+++ config/routes.rb
@@ -62,8 +62,9 @@ NolotiroOrg::Application.routes.draw do
     get '/message/list', to: 'messages#list', as: 'messages_list'
     get '/message/show/:id/subject/:subject', to: 'messages#show', as: 'message_show'
     # TODO
-    get '/message/create/id_user_to/:user_id/subject/:subject', to: "message#create", as: 'message_create'
-    post '/message/reply/:id/to/:message_id', to: 'message#reply', as: 'message_reply'
+    get '/message/create/id_user_to/:user_id', to: 'messages#create', as: 'message_create'
+    get '/message/create/id_user_to/:user_id/subject/:subject', to: "messages#create", as: 'message_create_with_subject'
+    post '/message/reply/:id/to/:message_id', to: 'messages#reply', as: 'message_reply'
 
     # rss
     # FIXME: las URLs legacy vienen asi
