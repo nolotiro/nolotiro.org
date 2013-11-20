7dc8181a24d64dc14cd6c0a0ce2673d746e94866
Andrés Pereira <apereira@alabs.es>
2013-11-20 10:33:18 +0100

    test: fixtures, tests for geo, user, etc

--- test/helpers/geo_helper_test.rb
+++ test/helpers/geo_helper_test.rb
@@ -1,12 +1,30 @@
 require 'test_helper'
 
+# 
+class TestMockupRequest
+
+  def initialize(remote_ip, http_x_forwarded_for) 
+    @remote_ip = remote_ip
+    @http_x_forwarded_for = http_x_forwarded_for
+  end
+
+  def remote_ip
+    @remote_ip
+  end
+
+  def env
+    @http_x_forwarded_for
+  end
+
+end
+
 class GeoHelperTest < ActionView::TestCase
   include GeoHelper
 
   test "should_get_ip_address" do
-    request = {}
+    request = TestMockupRequest.new
     ip = GeoHelper.get_ip_address request
-    assert_equal(ip, "87.218.80.79")
+    assert_equal(ip, "87.223.138.147")
   end 
 
   test "should_get_ip_address_with_http_x_forwared_for" do
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::TestHelpers
+
+  test "don't sign in a locked user" do
+    sign_in users(:three)
+  end
+  
+  test "let sign in a non locked user" do
+    sign_in users(:one)
+  end
+
 end
