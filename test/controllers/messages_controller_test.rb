b3e4e34e0377eeade276afcd277c1901e22333ed
Andrés Pereira <apereira@alabs.es>
2013-11-21 09:08:50 +0100

    tests: failed for messages

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class MessagesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get show of a given conversation" do
+    assert false
+  end
+
+  test "should create a message" do
+    assert false
+  end
+
+  test "should reply in a conversation" do
+    assert false
+  end
+
+  test "should get list of conversations" do
+    assert false
+  end
+
+
 end
