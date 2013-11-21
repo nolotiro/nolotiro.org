d0e7302ec7fcaf358d33771583d45ae9349b4a16
Andrés Pereira <apereira@alabs.es>
2013-11-21 12:17:34 +0100

    helpers: refactoring get_location_options and no_results

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def get_location_options(woeid)
+    # receives a woeid, returns a list of names like it
+    WoeidHelper.search_by_name(WoeidHelper.convert_woeid_name(woeid).split(',')[0])
+  end
+
 end
