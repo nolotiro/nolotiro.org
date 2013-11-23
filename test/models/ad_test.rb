2753f23c44f18d8530286fa5eecaad13ea327d9a
Andrés Pereira <apereira@alabs.es>
2013-11-23 13:43:39 +0100

    tests: models validations

--- test/models/ad_test.rb
+++ test/models/ad_test.rb
@@ -1,7 +1,105 @@
 require 'test_helper'
 
 class AdTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "ad requires title" do
+    a = Ad.new
+    a.valid?
+    assert a.errors[:title].include?("can't be blank")
+  end
+
+  test "ad requires body" do
+    a = Ad.new
+    a.valid?
+    assert a.errors[:body].include?("can't be blank")
+  end
+
+  test "ad requires user_owner" do
+    a = Ad.new
+    a.valid?
+    assert a.errors[:user_owner].include?("can't be blank")
+  end
+
+  test "ad requires type" do
+    a = Ad.new
+    a.valid?
+    assert a.errors[:type].include?("can't be blank")
+  end
+
+  test "ad requires woeid_code" do
+    a = Ad.new
+    a.valid?
+    assert a.errors[:woeid_code].include?("can't be blank")
+  end
+
+  test "ad requires date_created" do
+    a = Ad.new
+    a.valid?
+    assert a.errors[:date_created].include?("can't be blank")
+  end
+
+  test "ad requires ip" do
+    a = Ad.new
+    a.valid?
+    assert a.errors[:ip].include?("can't be blank")
+  end
+
+  test "ad requires status " do
+    a = Ad.new
+    a.valid?
+    assert a.errors[:status].include?("can't be blank")
+  end
+
+  test "ad validates type " do
+    # 1 2
+    a = Ad.new
+    a.valid?
+    assert a.errors[:].include?("can't be blank")
+    assert false
+  end
+
+  test "ad validates status " do
+    # 1 2 3
+    a = Ad.new
+    a.valid?
+    assert a.errors[:].include?("can't be blank")
+    assert false
+  end
+
+  test "ad validates ip " do
+    a = Ad.new
+    a.valid?
+    assert a.errors[:].include?("can't be blank")
+    assert false
+  end
+
+  test "ad check slug" do
+    a = Ad.new
+    a.valid?
+    assert a.errors[:].include?("can't be blank")
+    assert false
+  end
+
+  test "ad check type_string" do
+    a = Ad.new
+    a.valid?
+    assert a.errors[:].include?("can't be blank")
+    assert false
+  end
+
+  test "ad check status_string" do
+    a = Ad.new
+    a.valid?
+    assert a.errors[:].include?("can't be blank")
+    assert false
+  end
+
+  test "ad check status_class" do
+    a = Ad.new
+    a.valid?
+    assert a.errors[:].include?("can't be blank")
+    assert false
+  end
+
 end
+
