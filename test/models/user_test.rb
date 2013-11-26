095092127e5354172ba527d2f00d18735426634d
Andrés Pereira <apereira@alabs.es>
2013-11-26 11:49:00 +0100

    tests: user roles and default lang

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+
+  setup do
+    @ad = FactoryGirl.create(:ad)
+    @user = FactoryGirl.create(:user, "email" => "jaimito@gmail.com", "username" => "jaimito")
+    @admin = FactoryGirl.create(:admin)
+  end
+
+  test "should roles work" do 
+    assert_equal @user.admin?, false
+    assert_equal @admin.admin?, true
+  end
+
+  test "should default lang work" do
+    @user.lang = nil
+    @user.save
+    assert_equal @user.lang, 'es'
+  end
+
 end
