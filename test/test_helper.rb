c81e21c2116aa24de08670735b263a2999d7dbad
Andrés Pereira <apereira@alabs.es>
2013-11-29 06:08:04 +0100

    tests: fix bug in zeus tests running 2 times

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
+
+if ENV.keys.grep(/ZEUS/).any?
+  require 'minitest/unit'
+  MiniTest::Unit.class_variable_set("@@installed_at_exit", true)
+end
+
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
