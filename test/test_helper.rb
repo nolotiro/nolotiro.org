b40ba80107009c9efec75c4147744303468b370e
Andrés Pereira <apereira@alabs.es>
2013-12-23 11:38:16 +0100

    [ŧest] better gem turn config for short traces

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'minitest/spec'
 
+Turn.config.trace = 5
+
 if ENV.keys.grep(/ZEUS/).any?
   require 'minitest/unit'
   MiniTest::Unit.class_variable_set("@@installed_at_exit", true)
