b6ea8d0c751686b5cf316b7b56f2776d7a3fd1a7
Andrés Pereira <apereira@alabs.es>
2013-12-24 13:29:51 +0100

    routes with scopes

--- config/routes.rb
+++ config/routes.rb
@@ -20,23 +20,25 @@ NolotiroOrg::Application.routes.draw do
 
     resources :ads, path: 'ad', path_names: { new: 'create' }
     
-    # ads: optional slug
-    get '/ad/:id/:slug', to: 'ads#show', :as => 'adslug'
-    get '/ad/edit/id/:id', to: 'ads#edit', :as => 'ads_edit'
-
-    # listall
-    get '/ad/listall/ad_type/:type', to: 'woeid#show', as: "ads_listall" 
-    get '/ad/listall/ad_type/:type/status/:status', to: 'woeid#show', as: 'ads_listall_status'
+    scope '/ad' do
+      get '/:id/:slug', to: 'ads#show', :as => 'adslug'
+      get '/edit/id/:id', to: 'ads#edit', :as => 'ads_edit'
+      get '/listall/ad_type/:type', to: 'woeid#show', as: "ads_listall" 
+      get '/listall/ad_type/:type/status/:status', to: 'woeid#show', as: 'ads_listall_status'
+      get '/listuser/id/:id', to: 'users#listads', as: 'listads_user'
+    end
 
     # locations lists
     get '/woeid/:id/:type', to: 'woeid#show', as: 'ads_woeid'
     get '/woeid/:id/:type/status/:status', to: 'woeid#show', as: 'ads_woeid_status'
 
     # location change
-    get '/location/change', to: 'location#ask', as: 'location_ask'
-    post '/location/change/', to: 'location#list'
-    get '/location/change2/', to: 'location#list'
-    post '/location/change2/', to: 'location#change'
+    scope '/location' do
+      get '/change', to: 'location#ask', as: 'location_ask'
+      post '/change/', to: 'location#list'
+      get '/change2/', to: 'location#list'
+      post '/change2/', to: 'location#change'
+    end
 
     # auth
     get '/auth/login', to: redirect('/user/login')
@@ -59,7 +61,6 @@ NolotiroOrg::Application.routes.draw do
       get '/become/:id', to: 'admin#become', as: 'become_user' 
     end
 
-    get '/ad/listuser/id/:id', to: 'users#listads', as: 'listads_user'
     get '/user/edit/id/:id', to: redirect('/es/user/edit'), as: 'user_edit'
     get '/profile/:username', to: 'users#profile', as: 'profile'
 
@@ -70,28 +71,32 @@ NolotiroOrg::Application.routes.draw do
     get '/search', to: 'search#search', as: 'search'
 
     # messaging
-    get '/message/received', to: redirect('/es/message/list'), as: 'messages_received'
-    get '/message/list', to: 'messages#list', as: 'messages_list'
-    get '/message/show/:id/subject/:subject', to: 'messages#show', as: 'message_show'
-
-    get '/message/create/id_user_to/:user_id', to: 'messages#new', as: 'message_new'
-    get '/message/create/id_user_to/:user_id/subject/:subject', to: "messages#new", as: 'message_new_with_subject'
-    post '/message/create/id_user_to/:user_id', to: 'messages#create', as: 'message_create'
-    post '/message/create/id_user_to/:user_id/subject/:subject', to: "messages#create", as: 'message_create_with_subject'
-
-    post '/message/reply/:id/to/:user_id', to: 'messages#reply', as: 'message_reply'
+    scope '/message' do
+      get '/received', to: redirect('/es/message/list'), as: 'messages_received'
+      get '/list', to: 'messages#list', as: 'messages_list'
+      get '/show/:id/subject/:subject', to: 'messages#show', as: 'message_show'
+      get '/create/id_user_to/:user_id', to: 'messages#new', as: 'message_new'
+      get '/create/id_user_to/:user_id/subject/:subject', to: "messages#new", as: 'message_new_with_subject'
+      post '/create/id_user_to/:user_id', to: 'messages#create', as: 'message_create'
+      post '/create/id_user_to/:user_id/subject/:subject', to: "messages#create", as: 'message_create_with_subject'
+      post '/reply/:id/to/:user_id', to: 'messages#reply', as: 'message_reply'
+    end
 
     # rss
     # FIXME: las URLs legacy vienen asi
     # /en                     /rss/feed/woeid/766273                     /ad_type/give
-    get '/rss/feed/woeid/:woeid/ad_type/:type', format: 'rss', to: 'rss#feed', as: 'rss_type'
-    get '/rss/feed/woeid/:woeid/ad_type/give/status/:status', format: 'rss', to: 'rss#feed', as: 'rss_status'
+    scope '/rss' do
+      get '/feed/woeid/:woeid/ad_type/:type', format: 'rss', to: 'rss#feed', as: 'rss_type'
+      get '/feed/woeid/:woeid/ad_type/give/status/:status', format: 'rss', to: 'rss#feed', as: 'rss_status'
+    end
 
-    get '/page/faqs', to: 'page#faqs', as: 'faqs'
-    get '/page/tos', to: 'page#tos', as: 'tos'
-    get '/page/about', to: 'page#about', as: 'about'
-    get '/page/privacy', to: 'page#privacy', as: 'privacy'
-    get '/page/translate', to: 'page#translate', as: 'translate'
+    scope '/page' do
+      get '/faqs', to: 'page#faqs', as: 'faqs'
+      get '/tos', to: 'page#tos', as: 'tos'
+      get '/about', to: 'page#about', as: 'about'
+      get '/privacy', to: 'page#privacy', as: 'privacy'
+      get '/translate', to: 'page#translate', as: 'translate'
+    end
 
     # contact
     get '/contact', to: 'contact#new', as: 'contacts'
