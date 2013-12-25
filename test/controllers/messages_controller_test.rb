e27b2d72721e379fe28eeb7c7cac99fc8751c2ac
Andrés Pereira <apereira@alabs.es>
2013-12-25 21:36:34 +0100

    [tests] messages controller

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -11,33 +11,43 @@ class MessagesControllerTest < ActionController::TestCase
   end
 
   test "should redirect to signup to create a message as anon" do
-    get :create
+    get :new, user_id: @user1.id
     assert_redirected_to new_user_session_url
   end
 
   test "should get form to create a message as an user" do
     sign_in @user1
-    get :create
+    get :new, user_id: @user2.id
     assert_response :success
   end
 
-  test "should post a message to another user" do
+  test "should create a message, show it and let the other user reply it" do
+    # user1 signs in and sends a message to user2 
     sign_in @user1
-    post :create
+    message = { body: "lo sigues teniendo? ", subject: "interesado en el ordenador" }
+    expected_subject = message[:subject].parameterize
+    assert_difference('Message.count') do
+      post :create, user_id: @user2.id, message: message
+    end
+    assert_redirected_to message_show_path(id: assigns(:message), subject: expected_subject )
+    sign_out @user1
+    # TODO: assert flash 
+    # TODO: assert mailer 
+    # TODO: user2 signs in, read the message and reply it 
+    sign_in @user2
+    get :show, id: assigns(:message), subject: expected_subject
+    # TODO: user3 can't access that conversation
+  end
+
+  test "should get list of conversations as user" do
+    sign_in @user1
+    get :list
     assert_response :success
   end
 
-  test "should get show of a given conversation" do
-    assert false
+  test "should not get list of conversations as anon" do
+    get :list
+    assert_redirected_to new_user_session_url
   end
-
-  test "should reply in a conversation" do
-    assert false
-  end
-
-  test "should get list of conversations" do
-    assert false
-  end
-
 
 end
