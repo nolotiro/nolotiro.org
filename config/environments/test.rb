8fe829fc538af206c05b4cf05ca62f64bb277520
Andrés Pereira <apereira@alabs.es>
2013-12-25 21:39:08 +0100

    [ŧests] adding default_url_options to mailer for test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,8 @@ NolotiroOrg::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # for devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
