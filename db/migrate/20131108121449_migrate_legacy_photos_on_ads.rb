880396655f882b08ff9e559fd4e5a1c589c61552
Andrés Pereira <apereira@alabs.es>
2013-12-30 09:27:28 +0100

    [migrations] ads: photo migration cleanup

--- db/migrate/20131108121449_migrate_legacy_photos_on_ads.rb
+++ db/migrate/20131108121449_migrate_legacy_photos_on_ads.rb
@@ -1,37 +1,17 @@
 class MigrateLegacyPhotosOnAds < ActiveRecord::Migration
   def up
 
-    # missing photos :/
-    files = [
-      "35fa6b901391b306fec4ce241df300f4.jpg",
-      "795668875474b157f26169d567f70188.JPG",
-      "094ac5e4f303a85c667dc83628857e8c.jpg",
-      "1c93f6ee8a126455913226623cceb612.jpg",
-      "faede1067a93230f4193c140cee11daf.jpg",
-      "7979d7671e6cd39d9e8f4b73a7a0732f.JPG",
-      "e4536de7d08ccb7b3c7fcb0f5d2bbfea.JPG",
-    ]
-    # Touch missing photos
-    files.each do |f|
-      fi = File.open(Rails.root.to_s + "/public/legacy/uploads/ads/original/" + f, "w")
-      fi.close
-    end
-
     # For all Ads if they have photo we are going to the original photo file and 
     # save it as image; the photo attribute after the conversion is nil 
     Ad.all.find_each do |ad|
       if ad.photo? and ad.image_file_name.nil?
         filename = Rails.root.to_s + '/public/legacy/uploads/ads/original/' + ad.photo
         if File.file?(filename)
-          # From photo to image awww yeaaahhhh
           file = File.new(filename, "r")
           ad.image = file
           ad.photo = nil
           ad.save
           file.close
-        else
-          # List missing photos
-          puts filename
         end
       end
     end
