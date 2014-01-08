e774844b9572111f89cd7579c88c3eaaa3b8edd1
Andrés Pereira <apereira@alabs.es>
2014-01-08 11:05:10 +0100

    [ŧests] refactoring comment model validation

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,33 +2,12 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
 
-  test "comment requires ads_id" do
+  test "comment requires everything" do
     c = Comment.new
     c.valid?
     assert c.errors[:ads_id].include?("no puede estar en blanco")
-  end
-
-  test "comment requires body" do
-    c = Comment.new
-    c.valid?
     assert c.errors[:body].include?("no puede estar en blanco")
-  end
-
-  test "comment requires date_created" do
-    c = Comment.new
-    c.valid?
-    assert c.errors[:date_created].include?("no puede estar en blanco")
-  end
-
-  test "comment requires user_owner" do
-    c = Comment.new
-    c.valid?
     assert c.errors[:user_owner].include?("no puede estar en blanco")
-  end
-
-  test "comment requires ip" do
-    c = Comment.new
-    c.valid?
     assert c.errors[:ip].include?("no puede estar en blanco")
   end
 
