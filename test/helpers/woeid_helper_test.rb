1b3b610f7e670293fad3b2ae46fd070f8396f17a
Andrés Pereira <apereira@alabs.es>
2014-01-10 14:08:37 +0100

    bug fixing (no show adsense on development, update test with woeid helper change, add friend for anon user)

--- test/helpers/woeid_helper_test.rb
+++ test/helpers/woeid_helper_test.rb
@@ -10,7 +10,7 @@ class WoeidHelperTest < ActionView::TestCase
 
   test "should search serveral cities with the same name" do
     results = WoeidHelper.search_by_name "Bilbao" 
-    expected_results = [["Bilbao, País Vasco, España", 754542], ["Bilbao, Tolima, Colombia", 351483], ["Bilbao, Chimborazo, Ecuador", 56189784], ["Bilbao, Andalucía, España", 90231453], ["Bilbao (hda de Las Torres Univ), Chihuahua, México", 55885079], ["Bilbao, Bicolandia, Filipinas", 1163615], ["Bilbao, País Vasco, España", 90251343], ["Bilbao, Santiago del Estero, Argentina", 91831128]]
+    expected_results = [["Bilbao, País Vasco, España (0 anuncios)", 754542], ["Bilbao, Tolima, Colombia (0 anuncios)", 351483], ["Bilbao, Chimborazo, Ecuador (0 anuncios)", 56189784], ["Bilbao, Andalucía, España (0 anuncios)", 90231453], ["Bilbao (hda de Las Torres Univ), Chihuahua, México (0 anuncios)", 55885079], ["Bilbao, Bicolandia, Filipinas (0 anuncios)", 1163615], ["Bilbao, País Vasco, España (0 anuncios)", 90251343], ["Bilbao, Santiago del Estero, Argentina (0 anuncios)", 91831128]]
     assert_equal(expected_results, results)
   end
 
