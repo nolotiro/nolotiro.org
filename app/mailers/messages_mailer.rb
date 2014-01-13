bf25669427f3e103d89abfb80802fa479859a6c0
Andrés Pereira <apereira@alabs.es>
2014-01-13 11:20:03 +0100

    [messages] improving mailer

--- app/mailers/messages_mailer.rb
+++ app/mailers/messages_mailer.rb
@@ -3,11 +3,12 @@ class MessagesMailer < ActionMailer::Base
   default from: APP_CONFIG["default_from_email"]
 
   def create(to_email, from_username, subject, message)
+    @from_username = from_username
     @subject = subject
     @message = message
     mail(
       to: to_email,
-      subject: "[nolotiro.org] Tienes un nuevo mensaje del usuario #{from_username}"
+      subject: "[nolotiro.org] Tienes un nuevo mensaje del usuario #{@from_username}"
     )
   end
 
