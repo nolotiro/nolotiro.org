8fdffaeb417ee9e6894c99122c4c4453c9785712
Andrés Pereira <apereira@alabs.es>
2014-05-22 14:23:01 +0200

    [ads] eager loading for ad

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -84,7 +84,7 @@ class AdsController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_ad
-    @ad = Ad.find(params[:id])
+    @ad = Ad.includes(:comments).includes(:user).find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
