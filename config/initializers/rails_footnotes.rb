59e64c3dbac000537a1ea8db27e1892c07bc7edb
Andrés Pereira <apereira@alabs.es>
2014-05-22 14:22:23 +0200

    [gems] rails footnotes update

--- config/initializers/rails_footnotes.rb
+++ config/initializers/rails_footnotes.rb
@@ -1,5 +1,27 @@
-if defined?(Footnotes) && Rails.env.development?
-  Footnotes.run! # first of all
+defined?(Footnotes) && Footnotes.setup do |f|
+  # Wether or not to enable footnotes
+  f.enabled = Rails.env.development?
+  # You can also use a lambda / proc to conditionally toggle footnotes
+  # Example :
+  # f.enabled = -> { User.current.admin? }
+  # Beware of thread-safety though, Footnotes.enabled is NOT thread safe
+  # and should not be modified anywhere else.
 
-  # ... other init code
+  # Only toggle some notes :
+  # f.notes = [:session, :cookies, :params, :filters, :routes, :env, :queries, :log, :general]
+
+  # Change the prefix :
+  # f.prefix = 'mvim://open?url=file://%s&line=%d&column=%d'
+
+  # Disable style :
+  # f.no_style = true
+
+  # Lock notes to top right :
+  f.lock_top_right = true
+
+  # Change font size :
+  # f.font_size = '11px'
+
+  # Allow to open multiple notes :
+  # f.multiple_notes = true
 end
