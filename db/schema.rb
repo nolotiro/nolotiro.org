fc136b2bc3f82c42efb5e4a4588b8dab24c0f285
Andrés Pereira <apereira@alabs.es>
2014-05-23 09:35:04 +0200

    [messages] user deleted as user [borrado]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140522073916) do
+ActiveRecord::Schema.define(version: 20140522120854) do
 
   create_table "ads", force: true do |t|
     t.string   "title",              limit: 100, null: false
