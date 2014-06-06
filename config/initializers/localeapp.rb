683760787bfa1ceee66428cb274c25fe3742e4d8
Andrés Pereira <apereira@alabs.es>
2014-06-06 10:53:15 +0200

    [i18n] localeapp only when I said so

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -2,4 +2,7 @@ require 'localeapp/rails'
 
 Localeapp.configure do |config|
   config.api_key = APP_CONFIG["localeapp_apikey"]
+  config.sending_environments = []
+  config.reloading_environments = []
+  config.cache_missing_translations = true
 end
