158386a6f775c1c6b3557d1ea56a576da3bacd3e
Andrés Pereira <apereira@alabs.es>
2015-02-25 13:43:16 +0100

    [cache] better key with md5/current_user, fixing 'Too large filename'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,9 @@ module ApplicationHelper
     WoeidHelper.search_by_name(WoeidHelper.convert_woeid_name(woeid).split(',')[0])
   end
 
+  def cache_key_for(key, current_user)
+    key += current_user ? "user_" + current_user.admin?.to_s : "user_nil"
+    Digest::MD5.hexdigest(key)
+  end
+
 end
