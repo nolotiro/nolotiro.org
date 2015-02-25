e5dbcffb12c690b2f417da9aa17c7bbee77d73d5
Andrés Pereira <apereira@alabs.es>
2015-02-25 13:52:54 +0100

    [deploy] fixing git branches

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@ set :stage, :production
 set :rails_env, 'production' 
 server 'beta.nolotiro.org', user: 'ruby-data', roles: %w{db web app} #, my_property: :my_value
 set :deploy_to, '/var/www/nolotiro.org'
-set :branch, "deploy"
+set :branch, "production"
