c41ccf6aaca3aed2b18be4e9d3295db1fffa784a
Andrés Pereira <apereira@alabs.es>
2015-03-10 13:03:23 +0100

    [deploy] staging rvm gemset

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,3 +3,4 @@ set :rails_env, 'staging'
 server 'beta.nolotiro.org', user: 'ruby-data', roles: %w{db web app} #, my_property: :my_value
 set :deploy_to, '/var/www/beta.nolotiro.org'
 set :branch, "staging"
+set :rvm_ruby_version, '2.0.0-p247@nolotiro-staging'
