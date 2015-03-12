a69bc562ba3de3b15838fc9ff2aa74733af4dace
Andrés Pereira <apereira@alabs.es>
2015-03-12 17:14:34 +0100

    [i18n] moment from now on client - fix  Revisar cache e incongruencias en fechas #3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,31 +13,41 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.cookiebar
+//= require moment-with-locales
 // require turbolinks
 //= require_tree .
 
 
 $(document).ready(function(){
 
+  // alert cookies (bottom) 
   // http://www.primebox.co.uk/projects/jquery-cookiebar/ 
   $.cookieBar({
     message: $('.js-cookie-message').html(),
     acceptText: 'OK'
   });
-
   if (! readCookie('alert-first-visit-close') )  { $('.js-alert-first-visit').show('slow'); } 
 
+  // alert first visit
   $('.js-close-first-visit').on('click', function(e){
     e.preventDefault();
     $(this).parent().hide('slow');
     createCookie('alert-first-visit-close', true, 90); 
   })
 
+  // alert welcome message
   if ( $('p.notice').length > 0 ) {
    if ( $('p.notice').html().trim() == "Bienvenido" ) {
      $('p.notice').hide(); 
      $('#main').prepend('<p class="success">' + $('.js-welcome-message').html() + " " + $('#js-username').html() + '</p>')
    }
   }
+
+  // converting dates to "a month ago"
+  var lang = $('.lang.active').data('langcode');
+  moment.locale(lang);
+  $('.js-moment').each( function(){
+    $(this).html(moment($(this).html(), "YYYY-MM-DD h:mm:ss").fromNow()); 
+  });
 
 }); 
