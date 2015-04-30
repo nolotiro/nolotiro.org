62e36dc0c192f52130e6a746d90a83113386d3f3
Andrés Pereira <apereira@alabs.es>
2015-04-30 10:57:58 +0200

    [ads] [users] better validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -30,6 +30,30 @@ class UserTest < ActiveSupport::TestCase
     assert user2.errors[:email].include? "ya está en uso"
   end
 
+  test "shouldn't let a password with 4 characters - minimal 5" do 
+    @user.password = "1234"
+    @user.password = "1234"
+    assert_not @user.valid?
+    assert @user.errors[:password].include? "es demasiado corto (5 caracteres mínimo)"
+  end
+
+  test "should let a password with 5 characters" do 
+    @user.password = "12345"
+    @user.password = "12345"
+    assert @user.valid?
+  end
+
+  test "shouldn't let a username with 2 characters - minimal 3" do 
+    @user.username = "12"
+    assert_not @user.valid?
+    assert @user.errors[:username].include? "es demasiado corto (3 caracteres mínimo)"
+  end
+
+  test "should let a username with 3 characters" do 
+    @user.username = "123"
+    assert @user.valid?
+  end
+
   test "should roles work" do 
     assert_equal @user.admin?, false
     assert_equal @admin.admin?, true
