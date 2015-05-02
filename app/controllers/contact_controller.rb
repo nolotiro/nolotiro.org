ec5eb1dd1b7f23137405113b4237682006c1180e
Andrés Pereira <apereira@alabs.es>
2015-05-02 19:53:54 +0200

    [i18n] [contact]  Actualizar info de páginas: #30  - Si el usuario está logueado no debería pedirle su email sino tomar sus datos automáticamente

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -10,6 +10,7 @@ class ContactController < ApplicationController
   # POST /contact
   def create
     @contact = Contact.new(params[:contact])
+    @contact.email = current_user.email if user_signed_in?
     if @contact.valid? and verify_recaptcha(:model => @contact, :message => t('nlt.captcha_error'))
       ContactMailer.contact_form(@contact.email, @contact.message, request).deliver_now
       redirect_to root_url, notice: t('nlt.contact_thanks')
