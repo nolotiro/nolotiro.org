119d694a9ec7c6e1a444909c02e676bef57fb290
Andrés Pereira <apereira@alabs.es>
2015-05-06 18:07:29 +0200

    [ads] [comments] count - fix No aparece el nḿero de comentarios #23 - performance fix counter_cache

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :user, foreign_key: 'user_owner' #, :counter_cache => true
-  belongs_to :ad, foreign_key: 'ads_id' #, :counter_cache => true
+  belongs_to :ad, foreign_key: 'ads_id', :counter_cache => true
   
   validates :ads_id, presence: true
   validates :body, presence: true
