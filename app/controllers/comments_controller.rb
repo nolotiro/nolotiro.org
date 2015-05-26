115331e0294569c4e7a4da897c78f3a386a0f931
Andrés Pereira <apereira@alabs.es>
2015-05-26 10:06:46 +0200

    [comments] fix  Error al poner comentario demasiado largo #79

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,20 +4,21 @@ class CommentsController < ApplicationController
 
   # POST /comment/create/ad_id/:id
   def create
-    comment = Comment.new({
+    @ad = Ad.find params[:id]
+    @comment = Comment.new({
       ads_id: params[:id],
       body: params[:body],
       user_owner: current_user.id,
       ip: request.remote_ip,
     })
-    if comment.save
-      if comment.ad.user != current_user
+    if @comment.save
+      if @comment.ad.user != current_user
         CommentsMailer.create(params[:id], params[:body]).deliver_later
       end
-      expire_action(controller: 'ads', action: 'show', ad: comment.ad )
+      expire_action(controller: 'ads', action: 'show', ad: @comment.ad )
       redirect_to(ad_path(params[:id]), notice: t('nlt.comments.flash_ok'))
     else
-      redirect_to(:back, flash: {error: t('nlt.comments.flash_ko')}) 
+      render template: "ads/show"
     end
   end
 
