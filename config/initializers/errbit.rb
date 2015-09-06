f07e9607789a246be143b5d74699dda8fbc3e64d
Andrés Pereira <apereira@alabs.es>
2015-09-05 21:46:42 -0500

    [deploy] errbit config simpler

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,18 +1,25 @@
-class AirbrakeDeliveryWorker
-  include Airbrake
-  include Sidekiq::Worker
-  sidekiq_options queue: :airbrake, retry: false
-
-  def perform(notice)
-    Airbrake.sender.send_to_airbrake notice
-  end
-end
-
 Airbrake.configure do |config|
   config.api_key = Rails.application.secrets.airbrake["apikey"]
   config.host    = Rails.application.secrets.airbrake["host"]
   config.port    = Rails.application.secrets.airbrake["port"]
-  config.async do |notice| 
-    AirbrakeDeliveryWorker.perform_async(notice.to_xml)
-  end
+  config.secure  = config.port == 443
 end
+
+#class AirbrakeDeliveryWorker
+#  include Airbrake
+#  include Sidekiq::Worker
+#  sidekiq_options queue: :airbrake, retry: false
+#
+#  def perform(notice)
+#    Airbrake.sender.send_to_airbrake notice
+#  end
+#end
+#
+#Airbrake.configure do |config|
+#  config.api_key = Rails.application.secrets.airbrake["apikey"]
+#  config.host    = Rails.application.secrets.airbrake["host"]
+#  config.port    = Rails.application.secrets.airbrake["port"]
+#  config.async do |notice| 
+#    AirbrakeDeliveryWorker.perform_async(notice.to_xml)
+#  end
+#end
