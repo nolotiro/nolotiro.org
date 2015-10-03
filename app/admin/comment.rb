05fed52b14462d52aa421c72037d3e584dd96d83
Andrés Pereira <apereira@alabs.es>
2015-10-03 13:00:22 -0500

    [admin] comment/admin_comments

---
+++ app/admin/comment.rb
@@ -0,0 +1,28 @@
+ActiveAdmin.register Comment, as: "AdComment" do
+
+  # permit_params
+
+  filter :created_at
+
+  #controller do
+  #  def scoped_collection
+  #    super.includes :ad, :user
+  #  end
+  #end
+
+  index do
+    selectable_column
+    column :ad
+    column :body
+    column :user
+    column :ip
+    column :created_at
+    actions
+  end
+
+  action_item :view, only: :show do 
+    link_to "Ver en la web", ad_path(ad_comment.ad)
+  end
+
+end
+
