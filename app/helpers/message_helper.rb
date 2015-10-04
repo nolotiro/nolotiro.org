2f23dc172b10623287a25abfe3488791094205ab
Andrés Pereira <apereira@alabs.es>
2015-10-04 10:25:25 -0500

    [messages] fixing bug Error en mensajería close #113

--- app/helpers/message_helper.rb
+++ app/helpers/message_helper.rb
@@ -1,12 +1,16 @@
 module MessageHelper
 
   def other_username( current_user, m )
-    sender = m.messages.first.recipients.first.username
-    reciever = m.messages.first.recipients.second.username
-    if current_user == sender
-      reciever
-    else
-      sender
+    if m.messages.first and m.messages.first.recipients.first and m.messages.first.recipients.second 
+      sender = m.messages.first.recipients.first.username
+      reciever = m.messages.first.recipients.second.username
+      if current_user == sender
+        reciever
+      else
+        sender
+      end
+    else 
+      "[borrado]"
     end
   end
 
