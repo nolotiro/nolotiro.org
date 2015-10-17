f7c480b46c3c04bc57f9771ee67fb89624db2e9a
Andrés Pereira <apereira@alabs.es>
2015-10-17 13:51:49 -0500

    [admin] mailboxer model improvement

--- app/admin/mailboxer_notification.rb
+++ app/admin/mailboxer_notification.rb
@@ -1,8 +1,19 @@
 ActiveAdmin.register Mailboxer::Notification do
+
   menu parent: "Mensajería"
+
   filter :created_at
+  filter :body
+  filter :subject
+  filter :sender_id, as: :string
+  filter :conversation_id, as: :string
+
   index do 
     selectable_column
+    column :subject
+    column :sender
+    column :conversation
     actions
   end
+
 end
