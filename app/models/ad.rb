7ed186a60ce5183a06de36702e6011e07343592d
Andrés Pereira <apereira@alabs.org>
2016-02-23 10:08:58 +0100

    [bug] fixes ads images on adblock

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -52,7 +52,8 @@ class Ad < ActiveRecord::Base
 
   has_attached_file :image,
     styles: {thumb: "100x90>"},
-    process_in_background: :image 
+    process_in_background: :image,
+    url: "/system/img/:id/:basename.:extension"
 
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
