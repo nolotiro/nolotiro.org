d062f0ee66f172379518f53fd49268ad6503a6bb
David Rodríguez <deivid.rodriguez@gmail.com>
2016-03-04 15:52:59 +0100

    [tests] Remove unnecessary stuff from test setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,12 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'minitest/spec'
+
 require 'minitest/pride'
 require 'minitest/rails/capybara'
-include Warden::Test::Helpers
 
 Capybara.javascript_driver = :webkit
-Warden.test_mode!
 DatabaseCleaner.strategy = :transaction
-#DatabaseCleaner.strategy = :truncation
 
 # Ensure sphinx directories exist for the test environment
 ThinkingSphinx::Test.init
