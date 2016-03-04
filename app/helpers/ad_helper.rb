d699fb3fc15e519943853d93be4b0d1fe6f8ac94
David Rodríguez <deivid.rodriguez@gmail.com>
2016-03-04 16:21:17 +0100

    [ads] [users] Replace old ranks with new ranks

--- app/helpers/ad_helper.rb
+++ app/helpers/ad_helper.rb
@@ -1,14 +1,6 @@
 # encoding : utf-8
 module AdHelper
 
-  def self.get_users_ranking(limit=20, last_week=false)
-    if last_week
-      User.last_week.order("ads_count DESC").select('username, ads_count, id').limit(limit)
-    else
-      User.order("ads_count DESC").select('username, ads_count, id').limit(limit)
-    end
-  end
-
   def self.get_locations_ranking(limit=20)
     Ad.give.group_by(&:woeid_code).map{ |w,a| [WoeidHelper.convert_woeid_name(w)[:full], w, a.count] }.sort_by{|k| k[2]}.reverse.take(limit) 
   end
