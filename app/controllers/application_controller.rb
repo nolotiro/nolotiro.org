bc2b2f70a05305d68f3bcfad8995b209c489bf12
Antonio Pardo <apardo@alabs.org>
2016-03-23 00:07:04 +0100

    si es usado por clases hijas, protected no private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,14 +47,12 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  private
+  protected
 
   def get_location_suggest 
     ip_address = GeoHelper.get_ip_address request
     GeoHelper.suggest ip_address
   end
-
-  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
