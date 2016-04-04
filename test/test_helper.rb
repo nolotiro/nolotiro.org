b321c915842f8b0f4ef174457b66de0ba7a8a24b
David Rodríguez <deivid.rodriguez@gmail.com>
2016-04-04 19:37:49 +0200

    [tests] Fix random test failure
    When running tests with seed 34007, we would get the following failure
    in "AuthenticatedAdListing#test_0003_lists reserved ads in users
    location in home page":
    
    ```
    ActionController::RoutingError: No route matches [GET] "/admin/jobs"
    ```
    
    This is because some tests wouldn't reset the session after themselves.
    To fix it, reset the session in a global after hook.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -34,4 +34,8 @@ end
 class ActionDispatch::IntegrationTest
   # Make the Capybara DSL available in all integration tests
   include Capybara::DSL
+
+  after do
+    Capybara.reset_sessions!
+  end
 end
