2fdebe8036a4b320a91e7c4ed59205aadcebb5c3
David Rodríguez <deivid.rodriguez@gmail.com>
2016-04-12 12:56:24 +0200

    [tests] Use same naming criteria as in other tests

--- test/integration/authenticated_ad_listing_test.rb
+++ test/integration/authenticated_ad_listing_test.rb
@@ -7,9 +7,9 @@ class AuthenticatedAdListing < ActionDispatch::IntegrationTest
   include Pagination
 
   before do
-    create(:ad, :in_mad, title: 'ava_mad_1', status: 1)
+    create(:ad, :in_mad, title: 'ava_mad1', status: 1)
     create(:ad, :in_bar, title: 'ava_bar', status: 1)
-    create(:ad, :in_mad, title: 'ava_mad_2', status: 1)
+    create(:ad, :in_mad, title: 'ava_mad2', status: 1)
     create(:ad, :in_mad, title: 'res_mad', status: 2)
     create(:ad, :in_mad, title: 'del_mad', status: 3)
 
@@ -20,13 +20,13 @@ class AuthenticatedAdListing < ActionDispatch::IntegrationTest
   end
 
   it 'lists first page of available ads in users location in home page' do
-    page.assert_selector '.ad_excerpt_home', count: 1, text: 'ava_mad_1'
+    page.assert_selector '.ad_excerpt_home', count: 1, text: 'ava_mad1'
   end
 
   it 'lists other pages of available ads in users location in home page' do
     with_pagination(1) { click_link 'siguiente' }
 
-    page.assert_selector '.ad_excerpt_home', count: 1, text: 'ava_mad_2'
+    page.assert_selector '.ad_excerpt_home', count: 1, text: 'ava_mad2'
   end
 
   it 'lists reserved ads in users location in home page' do
