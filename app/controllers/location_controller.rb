d4041f4072d2b688ed520418e40300ca2c1b3d5b
David Rodríguez <deivid.rodriguez@gmail.com>
2016-04-20 12:56:16 +0200

    Fix Style/AccessorMethodName offenses

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -11,7 +11,7 @@ class LocationController < ApplicationController
     if params[:location]
       locations = WoeidHelper.search_by_name(params[:location])
       if not locations.nil? and locations.count == 1
-        set_location locations[0].woeid
+        save_location locations[0].woeid
       else
         @locations = locations
       end
@@ -21,11 +21,11 @@ class LocationController < ApplicationController
   # POST /es/location/change2
   def change
     if positive_integer?(params[:location])
-      set_location params[:location]
+      save_location params[:location]
     else
       locations = WoeidHelper.search_by_name(params[:location])
       if locations 
-        set_location locations[0].woeid
+        save_location locations[0].woeid
       else
         redirect_to location_ask_path, alert: "Hubo un error con el cambio de su ubicación. Inténtelo de nuevo."
       end
@@ -34,7 +34,7 @@ class LocationController < ApplicationController
 
   private
 
-  def set_location woeid
+  def save_location woeid
     # receives woeid, set location for user
     if user_signed_in? 
       current_user.woeid = woeid
