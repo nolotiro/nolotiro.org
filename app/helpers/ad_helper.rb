04b62a8bec1ee1ac6e33ab9ef6a53bc3ad1d7662
David Rodríguez <deivid.rodriguez@gmail.com>
2016-04-21 20:23:44 +0200

    [ads] Improve location counts performance
    Several performance improvements here:
    * Filter first, then resolver woeid names only of filtered items.
    * Group by SQL.
    * Count by SQL.
    * Sort by SQL.

--- app/helpers/ad_helper.rb
+++ app/helpers/ad_helper.rb
@@ -2,7 +2,9 @@
 module AdHelper
 
   def self.get_locations_ranking(limit=20)
-    Ad.give.group_by(&:woeid_code).map{ |w,a| [WoeidHelper.convert_woeid_name(w)[:full], w, a.count] }.sort_by{|k| k[2]}.reverse.take(limit) 
+    Ad.unscoped.give.group(:woeid_code).order('count_id desc').limit(limit).count('id').map do |w, c|
+      [WoeidHelper.convert_woeid_name(w)[:full], w, c]
+    end
   end
 
 end
