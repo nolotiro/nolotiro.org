b47281ddb676c172810325a011bba0bf9c7cb3a0
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-01 17:49:34 -0300

    [geo] Keep all GeoliteCity DBs between deployments

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,8 +14,8 @@ set :deploy_via, :remote_cache
 
 set :ssh_options, { :forward_agent => true }
 
-set :linked_files, %w{config/database.yml config/secrets.yml config/newrelic.yml vendor/geolite/GeoLiteCity.dat}
-set :linked_dirs, %w{db/sphinx log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/legacy}
+set :linked_files, %w{config/database.yml config/secrets.yml config/newrelic.yml}
+set :linked_dirs, %w{db/sphinx log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/legacy vendor/geolite}
 
 set :keep_releases, 5
 
