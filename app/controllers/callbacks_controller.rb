d02683a5ea08f2be0bc929b17f2a871753e63701
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-03 13:54:43 -0300

    Extract omniauth hash to a helper method

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,15 +1,20 @@
 class CallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
-    user = User.from_omniauth(request.env["omniauth.auth"])
+    user = User.from_omniauth(oauth)
     sign_in user
     redirect_to root_url
   end
 
   def google_oauth2
-    user = User.from_omniauth(request.env["omniauth.auth"])
+    user = User.from_omniauth(oauth)
     sign_in user
     redirect_to root_url
   end
 
+  private
+
+  def oauth
+    request.env["omniauth.auth"]
+  end
 end
