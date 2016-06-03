abb9d0ba17c33734dbf6a2e2a6f3fe244f5d8539
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-03 13:55:33 -0300

    Handle the empty email case for Facebook sign up

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,9 +1,14 @@
 class CallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
-    user = User.from_omniauth(oauth)
-    sign_in user
-    redirect_to root_url
+    if oauth.info.email.nil?
+      redirect_to new_user_registration_path(username: oauth.info.name),
+                  alert: I18n.t('devise.failure.not_facebook_confirmed_email')
+    else
+      user = User.from_omniauth(oauth)
+      sign_in user
+      redirect_to root_url
+    end
   end
 
   def google_oauth2
