033558791844dc42d74eb1c5499b5771e4343fbe
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-06 10:32:20 -0300

    Normalize users's primary key type
    It was currently an unsigned int but this runs into trouble when trying
    to create foreign key references since the types don't match and mysql
    is very picky with foreign key types.
    
    This is probably not compatible with postgreSQL but current migrations
    don't even run from scratch, so we'll probably won't use them if
    migrating to postgreSQL. If fact the new state is probably more
    compatible with postgreSQL than it was, although postgreSQL does not
    seem as picky as MySQL regarding data types.

---
+++ db/migrate/20160606130724_normalize_users_id_data_type.rb
@@ -0,0 +1,13 @@
+class NormalizeUsersIdDataType < ActiveRecord::Migration
+  def up
+    execute <<-SQL.squish
+      ALTER TABLE users MODIFY id INT(11) NOT NULL AUTO_INCREMENT
+    SQL
+  end
+
+  def down
+    execute <<-SQL.squish
+      ALTER TABLE users MODIFY id INT(11) UNSIGNED NOT NULL AUTO_INCREMENT
+    SQL
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603152006) do
+ActiveRecord::Schema.define(version: 20160606130724) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
