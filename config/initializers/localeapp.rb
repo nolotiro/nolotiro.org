f73e3cc5a93ccde9c55ef0ec8c96065f5abfd3a5
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-06 20:17:13 -0300

    Merge localeapp configs to a single file

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -2,7 +2,11 @@ require_relative '../application'
 require 'localeapp/rails'
 
 Localeapp.configure do |config|
-  config.api_key = Rails.application.secrets["localeapp_apikey"]
+  config.api_key = if Rails.env.test?
+                     'a' * 50
+                   else
+                     Rails.application.secrets["localeapp_apikey"]
+                   end
 
   config.polling_environments = []
   config.sending_environments = []
