f9f49791be13cce3c3285c8c753379150eed904c
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-06 23:19:23 -0300

    More accurate failure message in some assertions

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -5,19 +5,19 @@ class ContactTest < ActiveSupport::TestCase
   test "contact requires message" do
     c = Contact.new
     c.valid?
-    assert c.errors[:message].include?(I18n.t('activerecord.errors.messages.blank'))
+    assert_includes c.errors[:message], I18n.t('activerecord.errors.messages.blank')
   end
 
   test "contact requires email" do
     c = Contact.new
     c.valid?
-    assert c.errors[:message].include?(I18n.t('activerecord.errors.messages.blank'))
+    assert_includes c.errors[:message], I18n.t('activerecord.errors.messages.blank')
   end
 
   test "contact validates email" do
     c = Contact.new :email => "bla", :message => "yeeeaaa"
     c.valid?
-    assert c.errors[:email].include?(I18n.t('activerecord.errors.messages.invalid'))
+    assert_includes c.errors[:email], I18n.t('activerecord.errors.messages.invalid')
   end
 
   test "contact send mail" do
