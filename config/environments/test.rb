1e6ccc891d76424758c2afd7092574feefccfc7e
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-13 09:24:14 -0300

    Restore `eager_load` to false in tests
    Setting it to true doesn't play nice with `thinking_sphinx`, it
    generates duplicate section in the configuration causing it to fail to
    start. See pat/thinking-sphinx#523.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,9 +8,8 @@ NolotiroOrg::Application.configure do
   config.cache_classes = true
 
   # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = true
+  # just for the purpose of running a single test.
+  config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance.
   config.serve_static_files  = true
