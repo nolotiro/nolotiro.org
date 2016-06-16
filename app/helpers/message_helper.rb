236fd5b2ea331d4f97b57dad1780416db07a71c4
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-16 18:59:24 -0300

    Refactor `interlocutor` helper method

--- app/helpers/message_helper.rb
+++ app/helpers/message_helper.rb
@@ -1,17 +1,10 @@
 module MessageHelper
 
   def interlocutor( current_user, c )
-    if c.recipients and c.recipients.first and c.recipients.second 
-      sender = c.recipients.first
-      reciever = c.recipients.second
-      if current_user == sender
-        reciever
-      else
-        sender
-      end
-    else 
-      "[borrado]"
-    end
+    other_user = c.recipients.find { |u| u != current_user }
+    return "[borrado]" unless other_user
+
+    other_user
   end
 
 end
