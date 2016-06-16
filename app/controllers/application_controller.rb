bd93e1dca9a612be4418bd988f692ba4d04b8602
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-16 05:25:02 -0300

    Recaptcha already has i18n support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -72,8 +72,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email, :password, :remember_me) }
   end
-
-  def recaptcha_error
-    I18n.t('recaptcha.errors.verification_failed')
-  end
 end
