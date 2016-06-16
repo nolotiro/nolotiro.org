37f98e88de1a5f3ef3fc4e58f5837e128dcda060
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-16 18:47:40 -0300

    Rename method to be less misleading
    It doesn't actually return a username but a User instance. The only
    reason it's working as expected is because the string representation
    of a user is the username.

--- app/helpers/message_helper.rb
+++ app/helpers/message_helper.rb
@@ -1,6 +1,6 @@
 module MessageHelper
 
-  def other_username( current_user, c )
+  def interlocutor( current_user, c )
     if c.recipients and c.recipients.first and c.recipients.second 
       sender = c.recipients.first
       reciever = c.recipients.second
