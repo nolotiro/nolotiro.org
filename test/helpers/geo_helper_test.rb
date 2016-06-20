4e965d5c068d5301459e17056db706bacd9df56e
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-20 13:40:20 -0300

    Remove should style from geo helper test names

--- test/helpers/geo_helper_test.rb
+++ test/helpers/geo_helper_test.rb
@@ -3,19 +3,19 @@ require 'test_helper'
 class GeoHelperTest < ActionView::TestCase
   include GeoHelper
 
-  test "should get ip address" do
+  test "gets ip address" do
     @request.headers["REMOTE_ADDR"] = "87.223.138.147"
     ip = GeoHelper.get_ip_address(@request)
 
     assert_equal("87.223.138.147", ip)
   end 
 
-  test "should suggest location with ip address" do
+  test "suggests location with ip address" do
     suggestion = GeoHelper.suggest "8.8.8.8"
     assert_equal("Mountain View, California, Estados Unidos", suggestion)
   end
 
-  test "should suggest properly translated locations" do
+  test "suggests properly translated locations" do
     I18n.locale = :en
     suggestion = GeoHelper.suggest "8.8.8.8"
     assert_equal("Mountain View, California, United States", suggestion)
