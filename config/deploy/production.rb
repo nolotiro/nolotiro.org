56de5e09756cce35f35d8b5558e8ae5e76bf3c1e
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-20 19:55:01 -0300

    Configure automatic deployment after CI

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,5 +7,9 @@ set :rails_env, 'production'
 set :deploy_to, '/var/www/nolotiro.org'
 set :branch, 'master'
 
+if File.exist?('config/deploy/rsa_key')
+  set :ssh_options, fetch(:ssh_options).merge(keys: ['config/deploy/rsa_key'])
+end
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
