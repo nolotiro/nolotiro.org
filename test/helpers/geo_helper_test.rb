adb6e0bd2134e73ee88c160f9c9e17c4aea09435
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-20 13:40:53 -0300

    Reword a couple of tests for clarity

--- test/helpers/geo_helper_test.rb
+++ test/helpers/geo_helper_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class GeoHelperTest < ActionView::TestCase
   include GeoHelper
 
-  test "gets ip address" do
+  test "extracts ip address from request headers" do
     @request.headers["REMOTE_ADDR"] = "87.223.138.147"
     ip = GeoHelper.get_ip_address(@request)
 
     assert_equal("87.223.138.147", ip)
   end 
 
-  test "suggests location with ip address" do
+  test "suggests location from ip address" do
     suggestion = GeoHelper.suggest "8.8.8.8"
     assert_equal("Mountain View, California, Estados Unidos", suggestion)
   end
