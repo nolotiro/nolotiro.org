0e863b74b670cf9d133b6b0e82af3bc158f3db9c
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-24 07:26:19 -0300

    Minor simplification
    Extract an instance variable in some test's before block for simplicity
    and shorter lines in the test.

--- test/integration/links_for_booked_ads_test.rb
+++ test/integration/links_for_booked_ads_test.rb
@@ -4,16 +4,19 @@ require 'support/web_mocking'
 class LinksForBookedAdsTest < ActionDispatch::IntegrationTest
   include WebMocking
 
-  before { @ad = FactoryGirl.create(:ad, status: 2, comments_enabled: true) }
+  before do
+    @ad = FactoryGirl.create(:ad, status: 2, comments_enabled: true)
+    @woeid_code = @ad.woeid_code
+  end
 
   it 'does not show message link in listings' do
-    visit ads_woeid_path(id: @ad.woeid_code, type: 'give', status: 'booked')
+    visit ads_woeid_path(id: @woeid_code, type: 'give', status: 'booked')
 
     refute page.has_content?('Envía un mensaje privado al anunciante')
   end
 
   it 'does not show message link in ads' do
-    mocking_yahoo_woeid_info(@ad.woeid_code) do
+    mocking_yahoo_woeid_info(@woeid_code) do
       visit ad_path(@ad)
 
       refute page.has_content?('Envía un mensaje privado al anunciante')
@@ -21,7 +24,7 @@ class LinksForBookedAdsTest < ActionDispatch::IntegrationTest
   end
 
   it 'shows comment form in ads' do
-    mocking_yahoo_woeid_info(@ad.woeid_code) do
+    mocking_yahoo_woeid_info(@woeid_code) do
       visit ad_path(@ad)
 
       assert page.has_content?('accede para escribir un comentario')
--- test/integration/links_for_delivered_ads_test.rb
+++ test/integration/links_for_delivered_ads_test.rb
@@ -4,16 +4,19 @@ require 'support/web_mocking'
 class LinksForDeliveredAds < ActionDispatch::IntegrationTest
   include WebMocking
 
-  before { @ad = FactoryGirl.create(:ad, status: 3, comments_enabled: true) }
+  before do
+    @ad = FactoryGirl.create(:ad, status: 3, comments_enabled: true)
+    @woeid_code = @ad.woeid_code
+  end
 
   it 'does not show message link in listings' do
-    visit ads_woeid_path(id: @ad.woeid_code, type: 'give', status: 'delivered')
+    visit ads_woeid_path(id: @woeid_code, type: 'give', status: 'delivered')
 
     refute page.has_content?('Envía un mensaje privado al anunciante')
   end
 
   it 'does not show message link in ads' do
-    mocking_yahoo_woeid_info(@ad.woeid_code) do
+    mocking_yahoo_woeid_info(@woeid_code) do
       visit ad_path(@ad)
 
       refute page.has_content?('Envía un mensaje privado al anunciante')
@@ -21,7 +24,7 @@ class LinksForDeliveredAds < ActionDispatch::IntegrationTest
   end
 
   it 'does not show comment link in ads' do
-    mocking_yahoo_woeid_info(@ad.woeid_code) do
+    mocking_yahoo_woeid_info(@woeid_code) do
       visit ad_path(@ad)
 
       refute page.has_content?('accede para escribir un comentario')
