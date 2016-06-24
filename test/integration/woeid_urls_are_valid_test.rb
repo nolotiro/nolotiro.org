e6d28c5c618d0289598a70461b4a7f859b6d0ddd
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-24 06:29:51 -0300

    Drop should style from woeid redirection tests

--- test/integration/woeid_urls_are_valid_test.rb
+++ test/integration/woeid_urls_are_valid_test.rb
@@ -2,17 +2,17 @@ require "test_helper"
 
 class WoeidUrlParamValid < ActionDispatch::IntegrationTest
 
-  it "should redirect if woeid is not type town" do
+  it "redirects if woeid is not type town" do
     get '/es/woeid/23424801/give' #woeid of Ecuador Country
     assert_redirected_to '/es/location/change'
   end
 
-  it "should redirect if woeid is not an integer" do
+  it "redirects if woeid is not an integer" do
     get '/es/woeid/234LOOOOOOOOOOOOL24801/give'
     assert_redirected_to '/es/location/change'
   end
 
-  it "should redirect if woeid is not a valid woeid" do
+  it "redirects if woeid is not a valid woeid" do
     get '/es/woeid/222222/give' #woeid does not exists
     assert_redirected_to '/es/location/change'
   end
