9c5208aa2ff411aefea73ad4c7bf84b77744a85f
David Rodríguez <deivid.rodriguez@gmail.com>
2016-06-28 18:20:47 -0300

    Remove old non-working site wide announcements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,28 +3,6 @@
 //= require jquery.cookiebar
 //= require moment-with-locales
 //= require_tree .
-//
-
-function rememeberClosedAlerts() {
-  //<div class="info js-alert-cookie hide" data-cookie-name="alert-first-visit">
-  //  <%= t('nlt.first_visit') %>
-  //  <a href="#" class="js-alert-close alert-close">x</a> 
-  //</div>                                                                                        
-  $('.js-alert-cookie').each( function(){
-    var $el = $(this);
-    var cookie = $(this).attr('data-cookie-name');
-    if (! readCookie(cookie) ) { $el.show('slow'); } 
-  });
-
-  $('.js-alert-close').on('click', function(e){
-    e.preventDefault();
-    var $alert = $(this).parent('.js-alert-cookie');
-    var cookie = $alert.attr('data-cookie-name'); 
-    $alert.hide('slow');
-    createCookie(cookie, true, 90); 
-  });
-
-}
 
 $(document).ready(function(){
 
@@ -34,17 +12,6 @@ $(document).ready(function(){
     message: $('.js-cookie-message').html(),
     acceptText: 'OK'
   });
-  if (! readCookie('alert-first-visit-close') )  { $('.js-alert-first-visit').show('slow'); } 
-
-  rememeberClosedAlerts(); 
-
-  // alert welcome message
-  if ( $('p.notice').length > 0 ) {
-   if ( $('p.notice').html().trim() == "Bienvenido" ) {
-     $('p.notice').hide(); 
-     $('#main').prepend('<p class="success">' + $('.js-welcome-message').html() + " " + $('#js-username').html() + '</p>')
-   }
-  }
 
   // converting dates to "a month ago"
   var lang = $('.lang.active').data('langcode');
