03f912ebc17c7bd4db5d2254d02fa20842be8486
David Rodríguez <deivid.rodriguez@gmail.com>
2016-07-05 20:49:38 -0300

    Fix Style/SpaceBeforeBlockBraces offenses

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 ActiveAdmin.register_page 'Dashboard' do
-  menu priority: 1, label: proc{ I18n.t('active_admin.dashboard') }
+  menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
-  content title: proc{ I18n.t('active_admin.dashboard') } do
+  content title: proc { I18n.t('active_admin.dashboard') } do
     # Here is an example of a simple dashboard with columns and panels.
     #
     columns do
