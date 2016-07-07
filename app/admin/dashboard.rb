c688ccd1454950942ed4dc9ebe6467580ee9f7b0
David Rodríguez <deivid.rodriguez@gmail.com>
2016-07-07 09:07:29 -0300

    Don't need to i18n "Dashboard"

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register_page 'Dashboard' do
-  menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
+  menu priority: 1
 
-  content title: proc { I18n.t('active_admin.dashboard') } do
+  content do
     # Here is an example of a simple dashboard with columns and panels.
     #
     columns do
