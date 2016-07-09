ca89cd21e57fa107f34efdfc45a2afa7018cf81b
David Rodríguez <deivid.rodriguez@gmail.com>
2016-07-09 12:32:15 -0300

    Remove message things from admin console

--- app/admin/mailboxer_conversation.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-ActiveAdmin.register Mailboxer::Conversation do
-  menu parent: 'Mensajería'
-  filter :created_at
-  index do
-    selectable_column
-    actions
-  end
-end
--- app/admin/mailboxer_receipt.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-ActiveAdmin.register Mailboxer::Receipt do
-  menu parent: 'Mensajería'
-  filter :created_at
-  index do
-    selectable_column
-    actions
-  end
-end
