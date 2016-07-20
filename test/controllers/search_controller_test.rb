5deefe5d954c8e137f4187550e071f4b14d31daa
David Rodríguez <deivid.rodriguez@gmail.com>
2016-07-20 19:36:50 -0300

    Drop weird wording in some tests

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -11,21 +11,21 @@ class SearchControllerTest < ActionController::TestCase
     @user = FactoryGirl.create(:user)
   end
 
-  test 'should work search with a WOEID param' do
+  test 'search works with a WOEID param' do
     mocking_yahoo_woeid_info(@ad.woeid_code) do
       get :search, q: 'ordenador', woeid: @ad.woeid_code
       assert_response :success
     end
   end
 
-  test 'should work search with a WOEID param - no results' do
+  test 'search works with a WOEID param - no results' do
     mocking_yahoo_woeid_info(@ad.woeid_code) do
       get :search, q: 'notfound', woeid: @ad.woeid_code
       assert_response :success
     end
   end
 
-  test 'should work search with a logged in user with a WOEID code' do
+  test 'search works with a logged in user with a WOEID code' do
     mocking_yahoo_woeid_info(@user.woeid) do
       sign_in @user
       get :search, q: 'ordenador'
@@ -33,7 +33,7 @@ class SearchControllerTest < ActionController::TestCase
     end
   end
 
-  test 'should search work without WOEID param nor logged user' do
+  test 'search works without WOEID param nor logged user' do
     get :search
     assert_response :redirect
     assert_redirected_to location_ask_path
