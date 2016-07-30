adce642e2ba70b70fb56dbcb036081c46a8f7474
David Rodríguez <deivid.rodriguez@gmail.com>
2016-07-30 15:50:57 -0300

    Allow editing usermame

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -64,5 +64,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :remember_me])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email, :password, :remember_me])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
