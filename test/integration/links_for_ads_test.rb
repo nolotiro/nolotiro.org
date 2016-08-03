b62ab415cda461d306beada464b0d209663f3ad9
David Rodríguez <deivid.rodriguez@gmail.com>
2016-08-03 18:38:07 -0300

    Remove unnecessary instance variable

--- test/integration/links_for_ads_test.rb
+++ test/integration/links_for_ads_test.rb
@@ -5,15 +5,12 @@ require 'support/web_mocking'
 class LinksForAds < ActionDispatch::IntegrationTest
   include WebMocking
 
-  before do
-    @ad = create(:ad, comments_enabled: true)
-    @woeid_code = @ad.woeid_code
-  end
+  before { @ad = create(:ad, comments_enabled: true) }
 
   it 'shows message link in available ads' do
     @ad.update(status: 1)
 
-    mocking_yahoo_woeid_info(@woeid_code) do
+    mocking_yahoo_woeid_info(@ad.woeid_code) do
       visit ad_path(@ad)
 
       assert_content 'Envía un mensaje privado al anunciante'
@@ -23,7 +20,7 @@ class LinksForAds < ActionDispatch::IntegrationTest
   it 'does not show message link in booked ads' do
     @ad.update(status: 2)
 
-    mocking_yahoo_woeid_info(@woeid_code) do
+    mocking_yahoo_woeid_info(@ad.woeid_code) do
       visit ad_path(@ad)
 
       refute page.has_content?('Envía un mensaje privado al anunciante')
@@ -33,7 +30,7 @@ class LinksForAds < ActionDispatch::IntegrationTest
   it 'does not show message link in delivered ads' do
     @ad.update(status: 3)
 
-    mocking_yahoo_woeid_info(@woeid_code) do
+    mocking_yahoo_woeid_info(@ad.woeid_code) do
       visit ad_path(@ad)
 
       refute page.has_content?('Envía un mensaje privado al anunciante')
