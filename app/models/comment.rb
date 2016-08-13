fb295154fa71cdf4b687d16aa79062bdeb2a2a5d
David Rodríguez <deivid.rodriguez@gmail.com>
2016-08-13 01:23:36 -0300

    Don't touch ad when creating a comment
    Why do that?

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ActiveRecord::Base
   include Hidable
 
   belongs_to :user, foreign_key: 'user_owner'
-  belongs_to :ad, foreign_key: 'ads_id', touch: true
+  belongs_to :ad, foreign_key: 'ads_id'
 
   validates :ads_id, presence: true
   validates :body, presence: true
