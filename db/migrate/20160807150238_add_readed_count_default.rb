a770fcd2db487be6d9ac65bf2c168e07d9ec8677
David Rodríguez <deivid.rodriguez@gmail.com>
2016-08-14 00:02:25 -0300

    Complete readed count default change
    This was an overlook on my part. I should've changed existing defaults in
    the migration. I run it manually after deployment, but I'm adding it here
    for completeness.

--- db/migrate/20160807150238_add_readed_count_default.rb
+++ db/migrate/20160807150238_add_readed_count_default.rb
@@ -3,9 +3,13 @@
 class AddReadedCountDefault < ActiveRecord::Migration
   def up
     change_column_default :ads, :readed_count, 0
+
+    execute 'UPDATE ads SET readed_count = 0 WHERE readed_count IS NULL'
   end
 
   def down
     change_column_default :ads, :readed_count, nil
+
+    execute 'UPDATE ads SET readed_count = NULL WHERE readed_count = 0'
   end
 end
