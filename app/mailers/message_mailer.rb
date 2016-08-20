2772024cb798d96acb48b780a2920f1d51f1f2b2
David Rodríguez <deivid.rodriguez@gmail.com>
2016-08-20 12:21:10 -0300

    Use a single template for new messages and replies
    There's no need for the extra complexity.

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -8,24 +8,8 @@ class MessageMailer < ActionMailer::Base
     @receiver = message.recipient
     @subject = message.subject
 
-    if message.conversation.messages.size > 1
-      reply_message_email
-    else
-      new_message_email
-    end
-  end
-
-  private
-
-  def new_message_email
     mail to: @receiver.email,
          subject: t('mailboxer.message_mailer.subject_new', subject: @subject),
          template_name: 'new_message_email'
-  end
-
-  def reply_message_email
-    mail to: @receiver.email,
-         subject: t('mailboxer.message_mailer.subject_reply', subject: @subject),
-         template_name: 'reply_message_email'
   end
 end
