41a62e2287f955b65e3af2ce5c4a560163548f3e
David Rodríguez <deivid.rodriguez@gmail.com>
2016-08-27 14:59:21 -0300

    Test choosing locations via integration tests
    So they're more likely to catch bugs like the one we're currently having
    in the future.

---
+++ test/integration/choosing_location_test.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+require 'test_helper'
+require 'integration/concerns/authentication'
+require 'support/web_mocking'
+
+class ChoosingLocationTest < ActionDispatch::IntegrationTest
+  include WebMocking
+  include Authentication
+
+  it 'suggests locations matching name' do
+    mocking_yahoo_woeid_similar('tenerife') do
+      choose_location('tenerife')
+
+      assert_text 'Santa Cruz de Tenerife, Islas Canarias, España (0 anuncios)'
+    end
+  end
+
+  it 'chooses between locations matching name' do
+    mocking_yahoo_woeid_similar('tenerife') do
+      choose_location('tenerife')
+
+      select 'Santa Cruz de Tenerife, Islas Canarias, España (0 anuncios)'
+      click_button 'Elige tu ubicación'
+
+      assert_text 'regalo - Santa Cruz de Tenerife, Islas Canarias, España'
+    end
+  end
+
+  it 'memorizes chosen location in the user' do
+    user = create(:user)
+    login_as user
+
+    mocking_yahoo_woeid_similar('tenerife') do
+      choose_location('tenerife')
+
+      select 'Santa Cruz de Tenerife, Islas Canarias, España (0 anuncios)'
+      click_button 'Elige tu ubicación'
+      logout
+      login_as user
+
+      assert_text 'regalo - Santa Cruz de Tenerife, Islas Canarias, España'
+    end
+  end
+
+  private
+
+  def choose_location(name)
+    visit location_ask_path
+    fill_in 'location', with: name
+    click_button 'Enviar'
+  end
+end
