336d1e0b87955e4629633f12b409a0a815be6b21
David Rodríguez <deivid.rodriguez@gmail.com>
2016-09-02 08:23:23 -0300

    Add locale column to announcements

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160825201238) do
+ActiveRecord::Schema.define(version: 20160902112213) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20160825201238) do
     t.datetime "ends_at"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.string   "locale",     limit: 255
   end
 
   create_table "blockings", force: :cascade do |t|
