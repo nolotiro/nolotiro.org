7867cff46c685e59fa7d8d9164e4af16b21f408d
David Rodríguez <deivid.rodriguez@gmail.com>
2016-09-04 16:20:07 -0300

    DRY up one test file

--- test/integration/links_for_ads_test.rb
+++ test/integration/links_for_ads_test.rb
@@ -6,35 +6,31 @@ require 'support/web_mocking'
 class LinksForAds < ActionDispatch::IntegrationTest
   include WebMocking
 
-  before { @ad = create(:ad) }
-
   it 'shows message link in available ads' do
-    @ad.update(status: :available)
-
-    mocking_yahoo_woeid_info(@ad.woeid_code) do
-      visit ad_path(@ad)
-
+    within_ad_page(create(:ad, :available)) do
       assert_text 'Envía un mensaje privado al anunciante'
     end
   end
 
   it 'does not show message link in booked ads' do
-    @ad.update(status: :booked)
-
-    mocking_yahoo_woeid_info(@ad.woeid_code) do
-      visit ad_path(@ad)
-
+    within_ad_page(create(:ad, :booked)) do
       assert_no_text 'Envía un mensaje privado al anunciante'
     end
   end
 
   it 'does not show message link in delivered ads' do
-    @ad.update(status: :delivered)
-
-    mocking_yahoo_woeid_info(@ad.woeid_code) do
-      visit ad_path(@ad)
-
+    within_ad_page(create(:ad, :delivered)) do
       assert_no_text 'Envía un mensaje privado al anunciante'
+    end
+  end
+
+  private
+
+  def within_ad_page(ad)
+    mocking_yahoo_woeid_info(ad.woeid_code) do
+      visit ad_path(ad)
+
+      yield
     end
   end
 end
