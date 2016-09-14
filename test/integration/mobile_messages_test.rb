457a3d9e81b4073c52bf31bd8aceffdeae3b1705
David Rodríguez <deivid.rodriguez@gmail.com>
2016-09-14 15:34:24 -0300

    Fix the cookie bar failure in messaging tests
    ```
    Capybara::Poltergeist::MouseEventFailed: Firing a click at co-ordinates
    [30.5, 433.5] failed. Poltergeist detected another element with CSS
    selector 'html body div#cookie-bar p' at this position. It may be
    overlapping the element you are trying to interact with. If you don't
    care about overlapping elements, try using node.trigger('click').
    ```

--- test/integration/mobile_messages_test.rb
+++ test/integration/mobile_messages_test.rb
@@ -6,4 +6,14 @@ require 'integration/concerns/messaging_tests'
 
 class MobileMessagesTest < MobileIntegrationTest
   include MessagingTests
+
+  #
+  # In mobile screens, sometimes the cookie-bar gets in the middle so we need
+  # to dismiss it first.
+  #
+  before do
+    visit root_path
+
+    within('#cookie-bar') { click_link 'OK' }
+  end
 end
