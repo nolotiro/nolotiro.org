f2d32c7b6abf8c10f59259d6698ba5f860e90cf1
David Rodríguez <deivid.rodriguez@gmail.com>
2016-09-17 19:15:42 -0300

    Rubocop driven renaming
    Trying to keep our lines under rubocop's limits.

--- test/integration/authenticated_ad_listing_test.rb
+++ test/integration/authenticated_ad_listing_test.rb
@@ -10,11 +10,11 @@ class AuthenticatedAdListing < ActionDispatch::IntegrationTest
   include Pagination
 
   around do |test|
-    create(:ad, :in_mad, title: 'ava_mad1', status: 1, published_at: 1.day.ago)
-    create(:ad, :in_bar, title: 'ava_bar', status: 1, published_at: 2.days.ago)
-    create(:ad, :in_mad, title: 'ava_mad2', status: 1, published_at: 3.days.ago)
-    create(:ad, :in_mad, title: 'res_mad', status: 2)
-    create(:ad, :in_mad, title: 'del_mad', status: 3)
+    create(:ad, :in_mad, title: 'avamad1', status: 1, published_at: 1.day.ago)
+    create(:ad, :in_bar, title: 'avabar', status: 1, published_at: 2.days.ago)
+    create(:ad, :in_mad, title: 'avamad2', status: 1, published_at: 3.days.ago)
+    create(:ad, :in_mad, title: 'resmad', status: 2)
+    create(:ad, :in_mad, title: 'delmad', status: 3)
 
     login_as create(:user, woeid: 766_273)
 
@@ -34,40 +34,40 @@ class AuthenticatedAdListing < ActionDispatch::IntegrationTest
   it 'lists first page of available ads everywhere in all ads page' do
     visit ads_listall_path(type: 'give')
 
-    assert_selector '.ad_excerpt_list', count: 1, text: 'ava_mad1'
+    assert_selector '.ad_excerpt_list', count: 1, text: 'avamad1'
   end
 
   it 'lists other pages of available ads everywhere in all ads page' do
     visit ads_listall_path(type: 'give')
     click_link 'siguiente'
 
-    assert_selector '.ad_excerpt_list', count: 1, text: 'ava_bar'
+    assert_selector '.ad_excerpt_list', count: 1, text: 'avabar'
   end
 
   it 'lists first page of available ads in users location in home page' do
     visit root_path
 
-    assert_selector '.ad_excerpt_list', count: 1, text: 'ava_mad1'
+    assert_selector '.ad_excerpt_list', count: 1, text: 'avamad1'
   end
 
   it 'lists other pages of available ads in users location in home page' do
     visit root_path
     click_link 'siguiente'
 
-    assert_selector '.ad_excerpt_list', count: 1, text: 'ava_mad2'
+    assert_selector '.ad_excerpt_list', count: 1, text: 'avamad2'
   end
 
   it 'lists reserved ads in users location in home page' do
     visit root_path
     click_link 'reservado'
 
-    assert_selector '.ad_excerpt_list', count: 1, text: 'res_mad'
+    assert_selector '.ad_excerpt_list', count: 1, text: 'resmad'
   end
 
   it 'lists delivered ads in users location in home page' do
     visit root_path
     click_link 'entregado'
 
-    assert_selector '.ad_excerpt_list', count: 1, text: 'del_mad'
+    assert_selector '.ad_excerpt_list', count: 1, text: 'delmad'
   end
 end
