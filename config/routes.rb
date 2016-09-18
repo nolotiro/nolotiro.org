5258b2e3b8f7c3d6cc03fd1544b0860940ec25c3
David Rodríguez <deivid.rodriguez@gmail.com>
2016-09-18 16:30:52 -0300

    Remove complexity from routes file
    By extracting a couple of methods.

--- config/routes.rb
+++ config/routes.rb
@@ -78,24 +78,27 @@ NolotiroOrg::Application.routes.draw do
     get '/user/edit/id/:id', to: redirect('/es/user/edit'), as: 'user_edit'
     get '/profile/:id', to: 'users#profile', as: 'profile'
 
+    def rewritten_woeid(query)
+      query[:woeid] || query[:woeid_code]
+    end
+
+    def rewritten_type(query)
+      if query[:ad_type].present?
+        query[:ad_type] == '2' ? 'want' : 'give'
+      elsif query[:type].present?
+        query[:type]
+      else
+        'give'
+      end
+    end
+
     # search
     get '/search', to: redirect { |params, request|
       raw_query = URI.escape(request.query_string)
       query = Rack::Utils.parse_query(raw_query).symbolize_keys
 
-      woeid = query[:woeid] || query[:woeid_code]
-      new_path = "#{params[:locale]}/woeid/#{woeid}"
-
-      type = if query[:ad_type].present?
-               query[:ad_type] == '2' ? 'want' : 'give'
-             elsif query[:type].present?
-               query[:type]
-             else
-               'give'
-             end
-
-      new_path = "#{new_path}/#{type}"
-
+      new_path = "#{params[:locale]}/woeid/#{rewritten_woeid(query)}"
+      new_path = "#{new_path}/#{rewritten_type(query)}"
       new_path = "#{new_path}/status/#{query[:status]}" if query[:status].present?
       new_path = "#{new_path}/page/#{query[:page]}" if query[:page].present?
       "#{new_path}?q=#{query[:q]}"
