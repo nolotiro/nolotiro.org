81ba865eaaa05a986e4d0a0135624e5711fa818a
David Rodríguez <deivid.rodriguez@gmail.com>
2016-09-18 20:24:40 -0300

    Replace instance variable with helper method
    It's memoized so there's no need to explicitly set it in the controller
    and it has a better name (I think).

--- app/helpers/rankings_helper.rb
+++ app/helpers/rankings_helper.rb
@@ -2,14 +2,14 @@
 
 module RankingsHelper
   def last_week_user_ranking
-    return User.top_last_week unless @id
+    return User.top_last_week unless current_woeid
 
-    User.top_city_last_week(@id)
+    User.top_city_last_week(current_woeid)
   end
 
   def overall_user_ranking
-    return User.top_overall unless @id
+    return User.top_overall unless current_woeid
 
-    User.top_city_overall(@id)
+    User.top_city_overall(current_woeid)
   end
 end
