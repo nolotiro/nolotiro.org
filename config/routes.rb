4c317cf901bd6128cff7f49d759d1d6e0ae37dc3
David Rodríguez <deivid.rodriguez@gmail.com>
2016-09-18 17:24:01 -0300

    Move complex URL rewriting logic to separate file

--- config/routes.rb
+++ config/routes.rb
@@ -78,31 +78,8 @@ NolotiroOrg::Application.routes.draw do
     get '/user/edit/id/:id', to: redirect('/es/user/edit'), as: 'user_edit'
     get '/profile/:id', to: 'users#profile', as: 'profile'
 
-    def rewritten_woeid(query)
-      query[:woeid] || query[:woeid_code]
-    end
-
-    def rewritten_type(query)
-      if query[:ad_type].present?
-        query[:ad_type] == '2' ? 'want' : 'give'
-      elsif query[:type].present?
-        query[:type]
-      else
-        'give'
-      end
-    end
-
     # search
-    get '/search', to: redirect { |params, request|
-      raw_query = URI.escape(request.query_string)
-      query = Rack::Utils.parse_query(raw_query).symbolize_keys
-
-      new_path = "#{params[:locale]}/woeid/#{rewritten_woeid(query)}"
-      new_path = "#{new_path}/#{rewritten_type(query)}"
-      new_path = "#{new_path}/status/#{query[:status]}" if query[:status].present?
-      new_path = "#{new_path}/page/#{query[:page]}" if query[:page].present?
-      "#{new_path}?q=#{query[:q]}"
-    }
+    get '/search', to: redirect(SearchUrlRewriter.new)
 
     # blocking
     resources :blockings, only: [:create, :destroy]
