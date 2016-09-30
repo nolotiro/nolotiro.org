b21d30c23dbce6bca95d445ca611594094377a58
David Rodríguez <deivid.rodriguez@gmail.com>
2016-09-30 17:14:16 -0300

    Pull up default in contact mailer
    For consistency with the other mailers.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ContactMailer < ActionMailer::Base
+  default to: Rails.application.secrets.emails['contact']
+
   def contact_form(email, message, request)
     @email = email
     @ip_address = RequestGeolocator.new(request).ip_address
@@ -8,7 +10,6 @@ class ContactMailer < ActionMailer::Base
     @message = message
     mail(
       from: email,
-      to: Rails.application.secrets.emails['contact'],
       subject: "nolotiro.org - contact from #{email}"
     )
   end
