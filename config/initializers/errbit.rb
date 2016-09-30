f9f0a5365098d427124218ecd582c5d671939421
David Rodríguez <deivid.rodriguez@gmail.com>
2016-09-30 17:26:05 -0300

    Remove commented out unused code

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -8,22 +8,3 @@ if Rails.env.production? || Rails.env.staging?
     config.secure  = config.port == 443
   end
 end
-
-# class AirbrakeDeliveryWorker
-#  include Airbrake
-#  include Sidekiq::Worker
-#  sidekiq_options queue: :airbrake, retry: false
-#
-#  def perform(notice)
-#    Airbrake.sender.send_to_airbrake notice
-#  end
-# end
-#
-# Airbrake.configure do |config|
-#  config.api_key = Rails.application.secrets.airbrake["apikey"]
-#  config.host    = Rails.application.secrets.airbrake["host"]
-#  config.port    = Rails.application.secrets.airbrake["port"]
-#  config.async do |notice|
-#    AirbrakeDeliveryWorker.perform_async(notice.to_xml)
-#  end
-# end
