47f1ecd7dca38c531b78fe3d1eb13857024c822c
David Rodríguez <deivid.rodriguez@gmail.com>
2016-09-30 18:40:42 -0300

    Remove unnecessary requires
    We don't use Rails secrets for this now, so we don't need to load the
    whole Rails environment.

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 if Rails.env.development?
-  require_relative '../application'
-  require 'localeapp/rails'
-
   Localeapp.configure do |config|
     config.api_key = ENV['LOCALEAPP_APIKEY']
 
