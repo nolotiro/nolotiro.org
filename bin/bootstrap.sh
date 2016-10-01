6c92830b606e9d3eb836477919584a0df1d937e4
David Rodríguez <deivid.rodriguez@gmail.com>
2016-10-01 16:35:18 -0300

    Whitespace

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -3,8 +3,10 @@
 # TODO: check installed packages
 debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password ${MYSQL_PASS}"
 debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password ${MYSQL_PASS}"
+
 # Servers
 apt-get install -y curl phantomjs redis-server mysql-server-5.5 libmysqlclient-dev sqlite3 libsqlite3-dev imagemagick
+
 # Para compilar Ruby con rbenv
 apt-get install -y git-core make build-essential libssl-dev libreadline6-dev zlib1g-dev libyaml-dev libssl-dev libc6-dev
 
@@ -16,8 +18,10 @@ EOF
 
 # install rbenv and ruby-build
 sudo -u vagrant -i git clone git://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
+
 sudo -u vagrant echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.profile
 sudo -u vagrant echo 'eval "$(rbenv init -)"' >> /home/vagrant/.profile
+
 sudo -u vagrant -i git clone git://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build
 
 # install required ruby versions
