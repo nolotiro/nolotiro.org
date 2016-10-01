ff130f66dc1f58e1a344df5095452df3de73ba4e
David Rodríguez <deivid.rodriguez@gmail.com>
2016-10-01 16:40:18 -0300

    Tweak comments

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,30 +1,35 @@
 #!/usr/bin/env bash
 
 # TODO: check installed packages
+
+# Prepare MySQL for unattended installation
 debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password ${MYSQL_PASS}"
 debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password ${MYSQL_PASS}"
 
-# Servers
+# Application dependencies
 apt-get install -y curl phantomjs redis-server mysql-server-5.5 libmysqlclient-dev sqlite3 libsqlite3-dev imagemagick
 
-# Para compilar Ruby con rbenv
+# Rbenv dependencies
 apt-get install -y git-core make build-essential libssl-dev libreadline6-dev zlib1g-dev libyaml-dev libssl-dev libc6-dev
 
+# Enable auto-login for MySQL
 cat > /home/vagrant/.my.cnf <<EOF
 [client]
 user = root
 password =
 EOF
 
-# install rbenv and ruby-build
+# Install rbenv
 sudo -u vagrant -i git clone git://github.com/sstephenson/rbenv.git /home/vagrant/.rbenv
 
+# Load rbenv on login
 sudo -u vagrant echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.profile
 sudo -u vagrant echo 'eval "$(rbenv init -)"' >> /home/vagrant/.profile
 
+# Install ruby-build
 sudo -u vagrant -i git clone git://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build
 
-# install required ruby versions
+# Install required ruby versions
 sudo -u vagrant -i rbenv install 2.3.1
 sudo -u vagrant -i rbenv global 2.3.1
 sudo -u vagrant -i gem install bundler
