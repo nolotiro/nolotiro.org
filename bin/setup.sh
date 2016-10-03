87797495534ee44158d3e675b754ca432dd2262c
David Rodríguez <deivid.rodriguez@gmail.com>
2016-10-03 01:27:21 -0300

    Check installed packages, nodejs was missing.

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,16 +2,18 @@
 
 MYSQL_PASS=sincondiciones
 
-# TODO: check installed packages
-
 # Prepare MySQL for unattended installation
 debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password ${MYSQL_PASS}"
 debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password ${MYSQL_PASS}"
+
+# Setup nodejs repo
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
 
 # Application dependencies
 apt-get install -y imagemagick \
                    libmysqlclient-dev \
                    mysql-server-5.5 \
+                   nodejs \
                    phantomjs \
                    redis-server
 
