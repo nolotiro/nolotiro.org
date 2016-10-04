c82278a7e9128b2c469c4cab78555d83fde96d94
David Rodríguez <deivid.rodriguez@gmail.com>
2016-10-03 22:56:30 -0300

    Remove unnecessary stuff from bootstrap script
    Most of this stuff was probably necessary or nice to have at some point.
    Now it's not.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
-# http://docs.vagrantup.com/v2/getting-started/provisioning.html
 
 # TODO: check installed packages
-apt-get update
 debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password password ${MYSQL_PASS}"
 debconf-set-selections <<< "mysql-server-5.5 mysql-server/root_password_again password ${MYSQL_PASS}"
 # Servers
 apt-get install -y curl phantomjs redis-server mysql-server-5.5 libmysqlclient-dev sqlite3 libsqlite3-dev imagemagick
 # Para compilar Ruby con rbenv
 apt-get install -y git-core make build-essential libssl-dev libreadline6-dev zlib1g-dev libyaml-dev libssl-dev libc6-dev
-# Borrar la cache de los paquetes
-apt-get clean
 
 cat > /home/vagrant/.my.cnf <<EOF
 [client]
@@ -24,23 +20,15 @@ sudo -u vagrant echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/vagrant/.pr
 sudo -u vagrant echo 'eval "$(rbenv init -)"' >> /home/vagrant/.profile
 sudo -u vagrant -i git clone git://github.com/sstephenson/ruby-build.git /home/vagrant/.rbenv/plugins/ruby-build
 
-# no rdoc for installed gems
-echo 'gem: --no-ri --no-rdoc' >> /home/vagrant/.gemrc
-
 # install required ruby versions
 sudo -u vagrant -i rbenv install 2.3.1
 sudo -u vagrant -i rbenv global 2.3.1
-sudo -u vagrant -i ruby -v
-sudo -u vagrant -i gem install bundler --no-ri --no-rdoc
+sudo -u vagrant -i gem install bundler
 sudo -u vagrant -i rbenv rehash
 
 cd /vagrant
 sudo -u vagrant /home/vagrant/.rbenv/shims/bundle install
 
-chown vagrant:vagrant config/*.yml
-
-# TODO: db:seeds
 sudo -u vagrant bin/rake db:drop
 sudo -u vagrant bin/rake db:setup
-sudo -u vagrant bin/rake ts:index ts:restart
 sudo -u vagrant bin/rake max_mind:extract
