d10aaf3265b06ef543eecd17f7c4753ddc071808
David Rodríguez <deivid.rodriguez@gmail.com>
2016-10-30 11:33:55 -0200

    Remove unnecessary comments

--- app/controllers/rss_controller.rb
+++ app/controllers/rss_controller.rb
@@ -3,10 +3,7 @@
 class RssController < ApplicationController
   layout false
 
-  # GET '/rss/feed/woeid/:woeid/ad_type/:type'
-  # GET '/rss/feed/woeid/:woeid/ad_type/give/status/:status'
   def feed
-    # rss filter
     ads = Ad
     ads = params[:type] == 'want' ? ads.want.available : ads.give
     ads = ads.by_woeid_code(params[:woeid])
