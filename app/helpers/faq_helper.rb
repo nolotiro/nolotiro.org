adad64e8f42c8e0b79dc464dc3d6a83da3a4a54b
David Rodríguez <deivid.rodriguez@gmail.com>
2016-10-30 10:56:46 -0200

    Convert FAqs to a bootstrap list

--- app/helpers/faq_helper.rb
+++ app/helpers/faq_helper.rb
@@ -2,10 +2,12 @@
 
 module FaqHelper
   def faqentry(question, answer)
-    content_tag(:strong) { "* #{question}" } +
-      tag(:br) +
-      answer +
-      tag(:br) +
-      tag(:br)
+    content_tag(:div, class: 'list-group-item') do
+      h4 = content_tag(:h4, class: 'list-group-item-heading') { question }
+
+      p = content_tag(:p, class: 'list-group-item-text') { answer }
+
+      h4 + p
+    end
   end
 end
