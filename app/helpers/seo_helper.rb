497a8723c52bab0f72b0ad5e529d79e2f93bc361
David Rodríguez <deivid.rodriguez@gmail.com>
2016-11-06 16:21:37 -0200

    Extract page title to a helper

--- app/helpers/seo_helper.rb
+++ app/helpers/seo_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module SeoHelper
+  def meta_title
+    content_tag(:title, "#{content_for(:meta_title)} - nolotiro.org")
+  end
+
   def meta_description
     general = t('nlt.meta_description')
     specific = content_for(:meta_description) if content_for?(:meta_description)
