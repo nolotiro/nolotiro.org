bc44fca62be872c6501e6bf0beb09fddf8f6df34
David Rodríguez <deivid.rodriguez@gmail.com>
2016-11-20 18:33:13 -0200

    Style. Visually separate setup from assertions

--- test/controllers/api/v1_controller_test.rb
+++ test/controllers/api/v1_controller_test.rb
@@ -19,6 +19,7 @@ module Api
             { 'woeid_id' => 766_273, 'woeid_name' => 'Madrid', 'ads_count' => 1 }
           ]
         }
+
         assert_equal(body_expected, body)
         assert_equal(766_273, body['locations'][0]['woeid_id'])
         assert_response :success
@@ -28,6 +29,7 @@ module Api
     test 'should get ad show on api v1' do
       get :ad_show, format: 'json', id: @ad
       body = JSON.parse(@response.body)
+
       assert_equal(766_273, body['woeid_code'])
       assert_equal('ordenador en Vallecas', body['title'])
       assert_response :success
@@ -37,6 +39,7 @@ module Api
       mocking_yahoo_woeid_info(766_273) do
         get :woeid_show, format: 'json', type: 'give', id: 766_273
         body = JSON.parse(@response.body)
+
         assert_equal('766273', body['woeid_id'])
         assert_equal('Madrid, Madrid, España', body['woeid_name'])
         assert_equal('ordenador en Vallecas', body['ads'][0]['title'])
