d8271aa70fbe9819f3d263706aa4809d6ec8a452
David Rodríguez <deivid.rodriguez@gmail.com>
2016-11-25 10:27:39 -0200

    Refactor rankings
    * Move all rankings logic to a single place.
    * Normalize they way they are rendered.

---
+++ app/models/ad_ranking.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class AdRanking
+  def initialize(ad_scope, name:, metric:, select_extras: [], map_extras: [])
+    @ad_scope = ad_scope
+    @name = name
+    @metric = metric
+    @select_extras = select_extras
+    @map_extras = map_extras
+  end
+
+  def ranked
+    Rails.cache.fetch("#{@name}-#{cache_digest}") do
+      @ad_scope.select(@metric, *@select_extras, "COUNT(#{@metric}) as n_ads")
+               .group(*@select_extras, @metric)
+               .order('n_ads DESC')
+               .limit(size)
+               .map do |entry|
+        [
+          entry.send(@metric),
+          *(@select_extras + @map_extras).map { |extra| entry.send(extra) },
+          entry.n_ads
+        ]
+      end
+    end
+  end
+
+  def full?
+    ranked.length == size
+  end
+
+  def size
+    20
+  end
+
+  private
+
+  def cache_digest
+    last_ad_publication = Ad.maximum(:published_at)
+    return '0' * 20 unless last_ad_publication
+
+    last_ad_publication.strftime('%d%m%y%H%M%s')
+  end
+end
--- app/views/api/v1/woeid_list.json.jbuilder
+++ app/views/api/v1/woeid_list.json.jbuilder
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-json.locations @section_locations do |loc|
-  json.woeid_id loc.woeid_code
-  json.woeid_name loc.woeid_name_short
-  json.ads_count loc.n_ads
+json.locations @section_locations do |woeid_id, woeid_name, n_ads|
+  json.woeid_id woeid_id
+  json.woeid_name woeid_name
+  json.ads_count n_ads
 end
