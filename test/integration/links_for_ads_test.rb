0470739590cec30e058ee1dce1a07d5eac1267b7
David Rodríguez <deivid.rodriguez@gmail.com>
2016-11-25 11:19:57 -0200

    Change "contact links" labels
    We prefer not to include the "anunciante" word since it make everything
    feel "commercial". Let's change it with the username.

--- test/integration/links_for_ads_test.rb
+++ test/integration/links_for_ads_test.rb
@@ -7,26 +7,32 @@ class LinksForAds < ActionDispatch::IntegrationTest
   include AdTestHelpers
 
   it 'shows message link in available ads' do
-    visit_ad_page(create(:ad, :available))
+    visit_ad_page_with_type(:available)
 
-    assert_text 'Envía un mensaje privado al anunciante'
+    assert_text 'Envía un mensaje privado a lisa'
   end
 
   it 'does not show message link in booked ads' do
-    visit_ad_page(create(:ad, :booked))
+    visit_ad_page_with_type(:booked)
 
-    assert_no_text 'Envía un mensaje privado al anunciante'
+    assert_no_text 'Envía un mensaje privado a lisa'
   end
 
   it 'does not show message link in delivered ads' do
-    visit_ad_page(create(:ad, :delivered))
+    visit_ad_page_with_type(:delivered)
 
-    assert_no_text 'Envía un mensaje privado al anunciante'
+    assert_no_text 'Envía un mensaje privado a lisa'
   end
 
   it 'shows message link in petition ads' do
-    visit_ad_page(create(:ad, :want))
+    visit_ad_page_with_type(:want)
 
-    assert_text 'Envía un mensaje privado al anunciante'
+    assert_text 'Envía un mensaje privado a lisa'
+  end
+
+  private
+
+  def visit_ad_page_with_type(type)
+    visit_ad_page(create(:ad, type, user: create(:user, username: 'lisa')))
   end
 end
