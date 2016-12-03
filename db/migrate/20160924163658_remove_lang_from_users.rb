4ea77060aaa7a99712070ca66685d8e66334ef66
David Rodríguez <deivid.rodriguez@gmail.com>
2016-12-03 10:41:34 -0200

    Fix migration not being properly reversible

--- db/migrate/20160924163658_remove_lang_from_users.rb
+++ db/migrate/20160924163658_remove_lang_from_users.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class RemoveLangFromUsers < ActiveRecord::Migration
-  def change
+  def up
     remove_column :users, :lang, :string, limit: 4, null: false
+  end
+
+  def down
+    add_column :users, :lang, :string, limit: 4
+
+    execute "UPDATE users SET lang = 'es'"
+
+    change_column_null :users, :lang, false
   end
 end
