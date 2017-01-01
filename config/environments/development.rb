49ae13cb722cce2b757b9a7e4eaf591e6993cf38
David Rodríguez <deivid.rodriguez@gmail.com>
2017-01-01 11:56:28 -0200

    Remove setting I don't really use
    It makes the development environment slower, so let's remove it.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,11 +26,6 @@ NolotiroOrg::Application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
-  config.assets.debug = true
-
   # For devise
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
