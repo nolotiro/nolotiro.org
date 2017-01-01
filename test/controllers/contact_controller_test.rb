02604e4bac02bdfd63e78782318e6a7829ddd785
David Rodríguez <deivid.rodriguez@gmail.com>
2017-01-01 08:22:12 -0200

    Separate setup from assertions with a blank line

--- test/controllers/contact_controller_test.rb
+++ test/controllers/contact_controller_test.rb
@@ -7,11 +7,13 @@ class ContactControllerTest < ActionController::TestCase
 
   test 'should get contact form' do
     get :new
+
     assert_response :success
   end
 
   test 'should verify recaptcha in contact form' do
     post :create, contact: { email: 'hola@mundo.com', message: 'hola mundo. hola mundo. hola mundo. hola mundo. hola mundo. hola mundo. hola mundo.' }
+
     assert_redirected_to root_path
     assert_equal I18n.t('nlt.contact_thanks'), flash[:notice]
   end
--- test/controllers/rss_controller_test.rb
+++ test/controllers/rss_controller_test.rb
@@ -9,26 +9,31 @@ class RssControllerTest < ActionController::TestCase
 
   test 'should get the feed for a WOEID' do
     get :feed, woeid: @ad.woeid_code, type: 'give', format: 'rss'
+
     assert_response :success
   end
 
   test 'should get the feed for a WOEID/give/available' do
     get :feed, woeid: @ad.woeid_code, type: 'give', format: 'rss'
+
     assert_response :success
   end
 
   test 'should get the feed for a WOEID/give/booked' do
     get :feed, woeid: @ad.woeid_code, type: 'give', status: 'booked', format: 'rss'
+
     assert_response :success
   end
 
   test 'should get the feed for a WOEID/give/delivered' do
     get :feed, woeid: @ad.woeid_code, type: 'give', status: 'delivered', format: 'rss'
+
     assert_response :success
   end
 
   test 'should get the feed for a WOEID/want' do
     get :feed, woeid: @ad.woeid_code, type: 'want', format: 'rss'
+
     assert_response :success
   end
 end
