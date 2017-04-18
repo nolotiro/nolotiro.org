e4710b6bf3d1704b9834ad22dba550940957edbb
David Rodríguez <deivid.rodriguez@gmail.com>
2017-04-18 12:10:43 -0300

    Use ActiveSupport::Concern to reduce complexity

--- app/models/concerns/statable.rb
+++ app/models/concerns/statable.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
+require 'active_support/concern'
+
 #
 # Funcionality related to model stats
 #
 module Statable
-  def self.included(base)
-    base.extend ClassMethods
+  extend ActiveSupport::Concern
 
-    base.class_eval do
-      scope :last_day, -> { where("#{@stats_column} >= ?", 1.day.ago) }
-      scope :last_week, -> { where("#{@stats_column} >= ?", 1.week.ago) }
-      scope :last_month, -> { where("#{@stats_column} >= ?", 1.month.ago) }
-      scope :last_year, -> { where("#{@stats_column} >= ?", 1.year.ago) }
-    end
+  included do
+    scope :last_day, -> { where("#{@stats_column} >= ?", 1.day.ago) }
+    scope :last_week, -> { where("#{@stats_column} >= ?", 1.week.ago) }
+    scope :last_month, -> { where("#{@stats_column} >= ?", 1.month.ago) }
+    scope :last_year, -> { where("#{@stats_column} >= ?", 1.year.ago) }
   end
 
-  module ClassMethods
+  class_methods do
     def counter_stats_for(column)
       @stats_column = column
     end
