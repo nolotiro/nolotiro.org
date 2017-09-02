78974a0ec6b5851e3d572813f7cb11524ba6d24f
David Rodríguez <deivid.rodriguez@riseup.net>
2017-09-02 20:43:59 +0200

    Check for controller too in AdConstraint

--- app/constraints/ad_constraint.rb
+++ app/constraints/ad_constraint.rb
@@ -7,23 +7,29 @@ class AdConstraint
   def matches?(request)
     params = request.path_parameters
 
-    return false unless valid_combination?(params[:type], params[:status])
+    return false unless valid_combination?(params[:controller], params[:type], params[:status])
 
     true
   end
 
   private
 
-  def valid_combination?(type, status)
+  def valid_combination?(controller, type, status)
     # rubocop:disable Style/WordArray
     [
-      [nil, nil],
-      ['want', nil],
-      ['give', nil],
-      ['give', 'available'],
-      ['give', 'booked'],
-      ['give', 'delivered']
-    ].include?([type, status])
+      ['users', nil, nil],
+      ['users', 'want', nil],
+      ['users', 'give', nil],
+      ['users', 'give', 'available'],
+      ['users', 'give', 'booked'],
+      ['users', 'give', 'delivered'],
+      ['woeid', nil, nil],
+      ['woeid', 'want', nil],
+      ['woeid', 'give', nil],
+      ['woeid', 'give', 'available'],
+      ['woeid', 'give', 'booked'],
+      ['woeid', 'give', 'delivered']
+    ].include?([controller, type, status])
     # rubocop:enable Style/WordArray
   end
 end
