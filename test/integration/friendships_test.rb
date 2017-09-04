bf6be967b3885c4c653508bcddf940c596d5da68
David Rodríguez <deivid.rodriguez@riseup.net>
2017-09-04 21:11:23 +0200

    Move user actions to the right of profile header

--- test/integration/friendships_test.rb
+++ test/integration/friendships_test.rb
@@ -5,24 +5,24 @@ require 'integration/concerns/authenticated_test'
 require 'support/web_mocking'
 
 class FriendshipsTest < AuthenticatedTest
-  before { @friend = create(:user, username: 'other') }
+  before { @friend = create(:user) }
 
   it "creates friendships from target user's profile" do
     visit profile_path(@friend.username)
-    click_link 'agregar other a tu lista de amigos'
+    click_link 'agregar a tu lista de amigos'
 
-    assert_selector 'a', text: 'eliminar other de tu lista de amigos'
-    assert_no_selector 'a', text: 'agregar other a tu lista de amigos'
+    assert_selector 'a', text: 'eliminar de tu lista de amigos'
+    assert_no_selector 'a', text: 'agregar a tu lista de amigos'
     assert_text 'Amigo agregado'
   end
 
   it "destroys friendships from target user's profile" do
     create(:friendship, user: @current_user, friend: @friend)
     visit profile_path(@friend.username)
-    click_link 'eliminar other de tu lista de amigos'
+    click_link 'eliminar de tu lista de amigos'
 
-    assert_no_selector 'a', text: 'eliminar other de tu lista de amigos'
-    assert_selector 'a', text: 'agregar other a tu lista de amigos'
+    assert_no_selector 'a', text: 'eliminar de tu lista de amigos'
+    assert_selector 'a', text: 'agregar a tu lista de amigos'
     assert_text 'Amigo eliminado'
   end
 end
