9a56ed2aa39bdb17f145dde2b6caa1369851e75b
David Rodríguez <deivid.rodriguez@riseup.net>
2017-09-05 12:23:28 +0200

    Refactor unused keys test
    * Look more like the officially provided test.
    * Show all unused keys when failing.

--- test/i18n_test.rb
+++ test/i18n_test.rb
@@ -2,12 +2,14 @@
 
 require 'i18n/tasks'
 
-class I18nTest < Minitest::Test
-  def test_does_not_have_unused_keys
-    unused_keys = I18n::Tasks::BaseTask.new.unused_keys
-    msg = "#{unused_keys.leaves.count} unused i18n keys, run " \
-          '`i18n-tasks unused` to show them'
+class I18nTest < ActiveSupport::TestCase
+  def setup
+    @i18n = I18n::Tasks::BaseTask.new
+  end
 
-    assert_empty unused_keys, msg
+  def test_no_unused_keys
+    unused_keys = @i18n.unused_keys
+
+    assert_empty unused_keys, "#{unused_keys.inspect} are unused"
   end
 end
