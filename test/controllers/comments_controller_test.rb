634c7085ced4302e15c4456301bfe0979a4a6659
David Rodríguez <deivid.rodriguez@riseup.net>
2017-09-05 20:12:04 +0200

    Remove redundant lines in tests (#594)

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -13,7 +13,6 @@ class CommentsControllerTest < ActionController::TestCase
     assert_difference('Comment.count', 0) do
       post :create, params: { ad_id: @ad.id, comment: { body: 'hola mundo' } }
     end
-    assert_response :redirect
     assert_redirected_to new_user_session_url
   end
 
@@ -22,7 +21,6 @@ class CommentsControllerTest < ActionController::TestCase
     assert_difference('Comment.count', 1) do
       post :create, params: { ad_id: @ad.id, comment: { body: 'hola mundo' } }
     end
-    assert_response :redirect
     assert_redirected_to adslug_path(@ad, slug: @ad.slug)
   end
 end
