9a118f16e1a38e90702030ed1bf52dfd2da7c2cd
Mijail Rondon <mijail@riseup.net>
2018-11-09 00:51:09 +0100

    change deploy config for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 
-server "nolotiro.org", user: "ruby-data", port: 2225, roles: %w[db web app]
-
-set :stage, :production
-set :rails_env, "production"
+server "prod-nolotiro", user: "ruby-data", roles: %w[db web app]
 set :branch, "master"
-
-set :linked_files, %w[.env.production]
-
-if File.exist?("config/deploy/rsa_key")
-  set :ssh_options, fetch(:ssh_options).merge(keys: ["config/deploy/rsa_key"])
-end
-
-set :rbenv_type, :user
+set :rails_env, "production"
